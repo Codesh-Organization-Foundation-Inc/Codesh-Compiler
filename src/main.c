6e1f1722aaf6b30e321b8bc2a777981f8a4b5404
StavWasPlayZ <stavsolomon101@gmail.com>
2025-08-21 14:37:56 +0300

    Add token_type.h

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <regex.h>
 
 int main(void) {
     printf("Hello, World!\n");
     return 0;
 }
+
+// https://www.geeksforgeeks.org/c/regular-expressions-in-c/
---
+++ src/token/token_type.h
@@ -0,0 +1,9 @@
+#ifndef TOKEN_TYPE_H
+#define TOKEN_TYPE_H
+
+enum token_type
+{
+
+};
+
+#endif //TOKEN_TYPE_H
