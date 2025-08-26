c075e305dd6e46ac88deb87df8468277dee8f90c
Elione <eliranbmo2008@gmail.com>
2025-08-26 16:50:19 +0300

    made read_file function in main.cpp to read the input file

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "lexer/regex.h"
 #include "token/token.h"
 
+#include <fstream>
+#include <sstream>
 #include <iostream>
+#include <string>
+
+std::string read_file(const std::string &file_name);
 
 int main(const int argc, char **const argv) {
     // const codesh::command_args args = codesh::parse_command(argc, argv);
@@ -14,5 +19,20 @@ int main(const int argc, char **const argv) {
     delete token_test;
     delete token_num;
 
+    std::cout << read_file("../rashi.amen") << std::endl;
     return 0;
+}
+
+std::string read_file(const std::string &file_name)
+{
+    std::ifstream file;
+    file.open(file_name);
+    if (!file.is_open())
+    {
+        std::cerr << "Error: could not open file" << std::endl;
+    }
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    file.close();
+    return buffer.str();
 }
