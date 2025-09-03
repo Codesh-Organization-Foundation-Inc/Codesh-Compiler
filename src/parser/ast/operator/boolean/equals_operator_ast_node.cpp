b68d422c5aef6c75368c6dc32cbbc5af327135af
Elione <eliranbmo2008@gmail.com>
2025-09-03 18:58:33 +0300

    added all ast node of math and boolean operators

---
+++ src/parser/ast/operator/boolean/equals_operator_ast_node.cpp
@@ -0,0 +1,18 @@
+#include "equals_operator_ast_node.h"
+
+#include "../../node_type.h"
+
+template <typename T>
+codesh::ast::op::equals_operator_ast_node<T>::equals_operator_ast_node(std::unique_ptr<typed_ast_node<T>> left,
+                                                                           std::unique_ptr<typed_ast_node<T>> right) :
+    impl::binary_ast_node<T>(std::move(left), std::move(right))
+{
+}
+template <typename T> codesh::ast::node_type codesh::ast::op::equals_operator_ast_node<T>::get_type() const
+{
+    return node_type::EQUALS;
+}
+template <typename T> T codesh::ast::op::equals_operator_ast_node<T>::evaluate() const
+{
+    return this->get_left() == this->get_right();
+}
---
+++ src/parser/ast/operator/math/addition_operator_ast_node.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "../../impl/binary_ast_node.h"
+
+namespace codesh::ast::op
+{
+template <typename T>
+class addition_operator_ast_node final : public impl::binary_ast_node<T>
+{
+  public:
+    addition_operator_ast_node(std::unique_ptr<typed_ast_node<T>> left, std::unique_ptr<typed_ast_node<T>> right);
+
+    [[nodiscard]] node_type get_type() const override;
+
+  private:
+    [[nodiscard]] T evaluate() const override;
+};
+}
+
