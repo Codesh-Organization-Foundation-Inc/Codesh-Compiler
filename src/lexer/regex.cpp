f57f9084bd6831fd9962410e1c381fb74e13c4d2
StavWasPlayZ <stavsolomon101@gmail.com>
2025-09-07 23:57:27 +0300

    Add Maqaf Elion to identifier regex match

--- src/lexer/regex.cpp
+++ src/lexer/regex.cpp
@@ -17,7 +17,7 @@ static const std::vector<std::pair<token_group, std::string>> TOKEN_REGEXES = {
     {token_group::LITERAL_STRING, R"(\"(?:[^\"\\]|\\.)*\")"},
     {token_group::LITERAL_CHAR, R"('(?:[^'\\]|\\.)')"},
 
-    {token_group::IDENTIFIER, R"(\w+)"},
+    {token_group::IDENTIFIER, R"([\w־]+)"},
     {token_group::INVALID, R"(\S+)"},
 };
 
