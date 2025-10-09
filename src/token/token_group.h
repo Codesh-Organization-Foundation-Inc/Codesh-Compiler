bd8e2563c56f5cfc10ca726bab475de8e7368cc4
StavWasPlayZ <stavsolomon101@gmail.com>
2025-10-09 17:43:09 +0300

    Make method declaration follow regular declaration format

--- src/token/token_group.h
+++ src/token/token_group.h
@@ -53,7 +53,7 @@ enum class token_group : int
     KEYWORD_IMPLEMENTS, // וירש את
     KEYWORD_NEW, // ויברא
 
-    KEYWORD_FUNCTION, // המעשה
+    KEYWORD_FUNCTION, // מעשה
     KEYWORD_FUNCTION_CALL, // ויעש
     KEYWORD_FUNCTION_TAKE_ARGS, // ויקח
 
