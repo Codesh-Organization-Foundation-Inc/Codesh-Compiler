325ac8239148f027d90b8a41a250aaed26e79a70
Elione <eliranbmo2008@gmail.com>
2025-10-31 23:45:43 +0200

    Remodeled the rest of the keywords and token groups

--- src/token/token_group.h
+++ src/token/token_group.h
@@ -18,131 +18,131 @@ enum class token_group : int
     OPEN_QUOTATION, // יקרא
     CLOSE_QUOTATION, // לאמר
 
-     KEYWORD_ORIGIN_COUNTRY, // וישב בארץ
-     KEYWORD_IMPORT, // ויבא את
-     KEYWORD_IMPORT_STATIC, // ויבא בכלל את
+    KEYWORD_ORIGIN_COUNTRY, // וישב בארץ
+    KEYWORD_IMPORT, // ויבא את
+    KEYWORD_IMPORT_STATIC, // ויבא בכלל את
 
-     KEYWORD_LET, // ויהי
-     KEYWORD_NAME, // ושמו/ושמה
-     KEYWORD_SHALL_BE, // היה/תהיה
-     KEYWORD_RETURN, // וישב
+    KEYWORD_LET, // ויהי
+    KEYWORD_NAME, // ושמו/ושמה
+    KEYWORD_SHALL_BE, // היה/תהיה
+    KEYWORD_RETURN, // וישב
 
-     KEYWORD_FUNCTION, // מעשה
-     KEYWORD_FUNCTION_CALL, // ויעש
+    KEYWORD_FUNCTION, // מעשה
+    KEYWORD_FUNCTION_CALL, // ויעש
 
-     KEYWORD_CLASS, // עצם
-     KEYWORD_ENUM, // מניין
-     KEYWORD_INTERFACE, // צלם
+    KEYWORD_CLASS, // עצם
+    KEYWORD_ENUM, // מניין
+    KEYWORD_INTERFACE, // צלם
 
-     KEYWORD_PUBLIC, // נגלה
-     KEYWORD_PRIVATE, //
-     KEYWORD_PROTECTED, // קדש
-     KEYWORD_STATIC, // כללי
-     KEYWORD_FINAL, // ימות ולא־יתחלף
+    KEYWORD_PUBLIC, // נגלה
+    KEYWORD_PRIVATE, // נחבא
+    KEYWORD_PROTECTED, // קדש
+    KEYWORD_STATIC, // לכל־
+    KEYWORD_FINAL, // ימות ולא־יתחלף
 
-//     OPERATOR_ADDITION, // ויוסף ל-
-//     OPERATOR_SUBTRACTION, // ויוחסר מן
-//     OPERATOR_MULTIPLICATION, // ויוכפל
-//     OPERATOR_DIVISION, // ויחולק
-//     OPERATOR_TIMES, // פי
-//     OPERATOR_MODULO, // ויושרה
-//
-//     OPERATOR_ADDITION_ASSIGNMENT, //ויסף
-//     OPERATOR_SUBTRACTION_ASSIGNMENT, // ויחסר
-//     OPERATOR_MULTIPLICATION_ASSIGNMENT, // ויכפל
-//     OPERATOR_DIVISION_ASSIGNMENT, // ויחלק
-//     OPERATOR_MODULO_ASSIGNMENT, // וישרה
-//
-//     OPERATOR_INCREMENT, // ויתקדם
-//     OPERATOR_DECREMENT, // ויפחת
-//
-//
-//     KEYWORD_ANNOTATE, // והיה
-//     KEYWORD_ANNOTATE_OVERRIDE, // ויגזול
-//
-//     PUNCTUATION_END_OP, // :
-//     PUNCTUATION_ARG_SEPARATOR, // ו־
-//     PUNCTUATION_DOT, // ל־
-//     PUNCTUATION_WILDCARD, // כולל
-//
-//     KEYWORD_ANNOTATION, // חותם
-//     KEYWORD_THIS, // אנכי
-//     KEYWORD_INSTANCE, // מזרע
-//     KEYWORD_IMPLEMENTS, // וירש את
-//     KEYWORD_NEW, // ויברא
-//
-//     KEYWORD_ABSTRACT, // רוחני
-//     KEYWORD_SUPER, // אבי
-//
-//     KEYWORD_VAR, // משתנה
-//     KEYWORD_INTEGER, // שלם
-//     KEYWORD_FLOAT, // צף
-//     KEYWORD_DOUBLE, // מצף
-//     KEYWORD_LONG, // רב
-//     KEYWORD_SHORT, // קצר
-//     KEYWORD_BYTE, // קצרצר
-//     KEYWORD_CHAR, // אות
-//     KEYWORD_STRING, // פסוק
-//     KEYWORD_BOOLEAN, // דבר־מה
-//     KEYWORD_NULL, // תהו
-//     KEYWORD_ARRAY, // כמסדר
-//
-//     KEYWORD_REPLACE, // ויחלף
-//
-//     KEYWORD_IF, // אם
-//     KEYWORD_ELSE, // אחרת
-//     KEYWORD_SWITCH, // מחלוקת
-//     KEYWORD_CASE, // כאשר
-//     KEYWORD_DEFAULT, // חדל
-//
-//     KEYWORD_TRUE, // אמן
-//     KEYWORD_FALSE, // כזב
-//
-//     OPERATOR_EQUALS, // שווה
-//     OPERATOR_NOT_EQUALS, // אינו־שווה
-//     OPERATOR_NOT, // שקרי־הוא
-//     OPERATOR_GREATER, // גדול
-//     OPERATOR_GREATER_EQUAL, // גדול־ושווה
-//     OPERATOR_LESS, // קטן
-//     OPERATOR_LESS_EQUAL, // קטן־ושווה
-//     OPERATOR_AND, // וגם
-//     OPERATOR_OR, // או
-//
-//     OPERATOR_AGAINST, // לנגד
-//
-//
-//     KEYWORD_FOR, // וימנה
-//     KEYWORD_FROM, // מן
-//     KEYWORD_TO, // עד
-//     KEYWORD_SKIP, // וידלג
-//     KEYWORD_DO, // עשה
-//     KEYWORD_WHILE, // בעוד
-//     KEYWORD_CONTINUE, // פסח
-//     KEYWORD_BREAK, // לך־לך
-//
-//
-//     COMMENT_MULTILINE, // כה הגה ה' לאמור: ... ויחדל:
-//     COMMENT_ONE_LINER, // כה הגה ה'
-//
-//     KEYWORD_TRY, // וינסה
-//     KEYWORD_CATCH, // ויתפוס
-//     KEYWORD_THROW, // וישלך
-//     KEYWORD_THROWS, // וישלכהו
-//
-//     KEYWORD_INDEX, // במקום
-//
-//
-//     // REGEX Matches
-//
-//     LITERAL_NUMBER_FLOAT, // x.y צף | x צף
-//     LITERAL_NUMBER_DOUBLE, // x.y
-//     LITERAL_NUMBER_INT, // x
-//     LITERAL_STRING, // "פסוק"
-//     LITERAL_CHAR, // 'אות'
-//
-//     IDENTIFIER, // Any other word, eg. variable1
-//
-//     INVALID
-// };
-//
-// }
+    KEYWORD_ANNOTATION, // חותם
+    KEYWORD_ANNOTATE, // והיה
+    KEYWORD_ANNOTATE_OVERRIDE, // ויגזול
+
+    KEYWORD_EXTENDS, //בן־
+    KEYWORD_IMPLEMENTS, // בצלם
+    KEYWORD_NEW, // ויברא
+    KEYWORD_INSTANCE, // מזרע
+    KEYWORD_ABSTRACT, // רוחני
+    KEYWORD_THIS, // אנכי
+    KEYWORD_SUPER, // אבי
+
+    OPERATOR_ADDITION, // ויוסף ל-
+    OPERATOR_SUBTRACTION, // ויוחסר מן
+    OPERATOR_MULTIPLICATION, // ויוכפל
+    OPERATOR_DIVISION, // ויחולק
+    OPERATOR_MODULO, // ויושרה
+    OPERATOR_BY, // פי־
+
+    OPERATOR_ADDITION_ASSIGNMENT, //ויסף
+    OPERATOR_SUBTRACTION_ASSIGNMENT, // ויחסר
+    OPERATOR_MULTIPLICATION_ASSIGNMENT, // ויכפל
+    OPERATOR_DIVISION_ASSIGNMENT, // ויחלק
+    OPERATOR_MODULO_ASSIGNMENT, // וישרה
+
+    OPERATOR_INCREMENT, // ויתקדם
+    OPERATOR_DECREMENT, // ויפחת
+
+    KEYWORD_VAR, // משתנה
+    KEYWORD_INTEGER, // שלם
+    KEYWORD_FLOAT, // צף
+    KEYWORD_DOUBLE, // מצף
+    KEYWORD_LONG, // רב
+    KEYWORD_SHORT, // קצר
+    KEYWORD_BYTE, // קצרצר
+    KEYWORD_CHAR, // אות
+    KEYWORD_BOOLEAN, // דבר־מה
+    KEYWORD_NULL, // תהו
+
+    KEYWORD_STRING, // פסוק
+
+    KEYWORD_ARRAY, // כמסדר
+    KEYWORD_INDEX, // במקום i
+    KEYWORD_REPLACE, // ויחלף
+    PUNCTUATION_EQUAL, // ב־
+
+    KEYWORD_IF, // אם־
+    KEYWORD_ELSE, // אחרת
+    KEYWORD_IF_ELSE, // אם־אחרת
+    KEYWORD_SWITCH, // ויחלוק על
+    KEYWORD_CASE, // כאשר
+    KEYWORD_DEFAULT, // באין צדק
+
+    KEYWORD_TRUE, // אמן
+    KEYWORD_FALSE, // כזב
+
+    OPERATOR_NOT, // שקרי־הוא
+    OPERATOR_EQUALS, // שווה
+    OPERATOR_NOT_EQUALS, // אינו־שווה
+    OPERATOR_GREATER, // גדול
+    OPERATOR_GREATER_EQUAL, // גדול־ושווה
+    OPERATOR_LESS, // קטן
+    OPERATOR_LESS_EQUAL, // קטן־ושווה
+    OPERATOR_AGAINST, // לנגד
+    OPERATOR_AND, // וגם
+    OPERATOR_OR, // או
+
+    PUNCTUATION_END_OP, // :
+    PUNCTUATION_ARG_SEPARATOR, // ו־
+    PUNCTUATION_DOT, // ל־
+    PUNCTUATION_WILDCARD, // כולל
+
+    KEYWORD_FOR, // וימנה
+    KEYWORD_FROM, // מן
+    KEYWORD_TO, // עד
+    KEYWORD_SKIP, // וידלג
+    KEYWORD_DO, // עשה
+    KEYWORD_WHILE, // בעוד
+    KEYWORD_CONTINUE, // פסח
+    KEYWORD_BREAK, // לך־לך
+
+    KEYWORD_TRY, // וינסה
+    KEYWORD_CATCH, // ויתפוס
+    KEYWORD_THROW, // גזר
+    KEYWORD_THROWS, // פן־יחטא
+
+    COMMENT_MULTILINE, // כה הגה ה' לאמור: ... ויחדל:
+    COMMENT_ONE_LINER, // כה הגה ה'
+
+
+
+
+    // REGEX Matches
+
+    LITERAL_NUMBER_FLOAT, // x.y צף | x צף
+    LITERAL_NUMBER_DOUBLE, // x.y
+    LITERAL_NUMBER_INT, // x
+    LITERAL_STRING, // "פסוק"
+    LITERAL_CHAR, // 'אות'
+
+    IDENTIFIER, // Any other word, eg. variable1
+
+    INVALID
+};
+
+}
