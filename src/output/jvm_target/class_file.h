085af4e813293b1fcd81e69d9a49c630178dfb8d
Elione <eliranbmo2008@gmail.com>
2025-11-03 21:32:57 +0200

    Made the cp_info file and necessary structs.

--- src/output/jvm_target/class_file.h
+++ src/output/jvm_target/class_file.h
@@ -1,7 +1,12 @@
 #pragma once
+#include "cp_info.h"
 
 namespace codesh::output::jvm_target
 {
+
+
+
+
 
 struct class_file
 {
@@ -9,7 +14,9 @@ struct class_file
     unsigned char minor_version[2];
     unsigned char major_version[2];
     unsigned char constant_pool_count[2];
+    std::vector<cp_info*> constant_pool;
 
 };
+
 
 }
