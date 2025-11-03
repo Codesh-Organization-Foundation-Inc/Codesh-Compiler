18047864771f6071d20957b7b11093ddb6035e1d
Elione <eliranbmo2008@gmail.com>
2025-11-03 20:48:37 +0200

    Added simple fields to class_file.h

--- src/output/jvm_target/class_file.h
+++ src/output/jvm_target/class_file.h
@@ -6,6 +6,10 @@ namespace codesh::output::jvm_target
 struct class_file
 {
     unsigned char magic[4];
+    unsigned char minor_version[2];
+    unsigned char major_version[2];
+    unsigned char constant_pool_count[2];
+
 };
 
 }
