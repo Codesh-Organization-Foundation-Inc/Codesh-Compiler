8584374f408bc02c6d2decefc1d372d563395e85
Elione <eliranbmo2008@gmail.com>
2025-11-12 18:29:36 +0200

    made the add_utf8 func and basic build func

--- src/output/jvm_target/build.cpp
+++ src/output/jvm_target/build.cpp
@@ -1,9 +1,41 @@
 #include "build.h"
 
+
+static void add_utf8(codesh::output::jvm_target::defs::class_file &cf, const std::string &s)
+{
+    auto const_utf8 = std::make_unique<codesh::output::jvm_target::defs::CONSTANT_Utf8_info>();
+    const_utf8->tag[0] = 1;
+    const_utf8->length[0] = s.size(); // length as 1 byte
+    // TODO: Add a check: if size > 255
+    const_utf8->bytes.insert(const_utf8->bytes.end(), s.begin(), s.end());
+    cf.constant_pool.push_back(std::move(const_utf8));
+}
+
 codesh::output::jvm_target::defs::class_file codesh::output::jvm_target::build(
     const ast::compilation_unit_ast_node *root_node)
 {
-    // TODO: Eliran
+    defs::class_file class_file;
+
+    // magic
+    class_file.magic[0] = 0xCA;
+    class_file.magic[1] = 0xFE;
+    class_file.magic[2] = 0xBA;
+    class_file.magic[3] = 0xBE;
+
+    // minor version
+    class_file.minor_version[0] = 0x00;
+    class_file.minor_version[1] = 0x00;
+
+    // major version
+    class_file.minor_version[0] = 0x00;
+    class_file.minor_version[1] = 0x41;
+
+    // constant pool
+    class_file.constant_pool_count[0] = 0x00;
+    class_file.constant_pool_count[0] = 0x10;
+
+
+
 }
 
 void codesh::output::jvm_target::write_to_file(const defs::class_file &class_file, std::string destination)
--- src/output/jvm_target/defs/class_file.h
+++ src/output/jvm_target/defs/class_file.h
@@ -11,18 +11,18 @@ struct class_file
     unsigned char magic[4];
     unsigned char minor_version[2];
     unsigned char major_version[2];
-    // unsigned char constant_pool_count[2];
+    unsigned char constant_pool_count[2];
     std::vector<std::unique_ptr<cp_info>> constant_pool;
     unsigned char access_flags[2];
     unsigned char this_class[2];
     unsigned char super_class[2];
     unsigned char interfaces_count[2];
-    // unsigned char interfaces_info[0]; // change later
+    unsigned char interfaces_info[0]; // change later
     unsigned char fields_count[2];
-    // unsigned char fields_info[0]; // change later
-    // unsigned char methods_count[2];
+    unsigned char fields_info[0]; // change later
+    unsigned char methods_count[2];
     std::vector<std::unique_ptr<methods_info_entry>> methods_info;
-    // unsigned char attribute_count[2];
+    unsigned char attribute_count[2];
     std::vector<std::unique_ptr<attribute_info_entry>> attribute_info;
 
 };
