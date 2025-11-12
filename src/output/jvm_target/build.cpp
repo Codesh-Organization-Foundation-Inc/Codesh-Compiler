cc3481f37d66f88303c7888f5de5a7ea799d70a0
StavWasPlayZ <stavsolomon101@gmail.com>
2025-11-12 10:33:58 +0200

    Add base build/compile functions

---
+++ src/output/jvm_target/build.cpp
@@ -0,0 +1,12 @@
+#include "build.h"
+
+codesh::output::jvm_target::defs::class_file codesh::output::jvm_target::build(
+    const ast::compilation_unit_ast_node *root_node)
+{
+    // TODO: Eliran
+}
+
+void codesh::output::jvm_target::write_to_file(const defs::class_file &class_file, std::string destination)
+{
+    // TODO: Eliran
+}
---
+++ src/output/jvm_target/build.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "defs/class_file.h"
+
+namespace codesh::ast
+{
+class compilation_unit_ast_node;
+}
+namespace codesh::output::jvm_target
+{
+
+[[nodiscard]] defs::class_file build(const ast::compilation_unit_ast_node *root_node);
+void write_to_file(const defs::class_file &class_file, std::string destination);
+
+}
