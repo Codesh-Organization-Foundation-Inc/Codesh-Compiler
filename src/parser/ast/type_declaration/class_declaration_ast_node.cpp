3f8faab8641a92d58105aab28162ad3310ae1c4d
StavWasPlayZ <stavsolomon101@gmail.com>
2025-11-15 01:49:51 +0200

    Implement extending class in class_declaration_ast_node

--- src/parser/ast/type_declaration/class_declaration_ast_node.cpp
+++ src/parser/ast/type_declaration/class_declaration_ast_node.cpp
@@ -4,3 +4,15 @@ codesh::ast::type_decl::class_declaration_ast_node::class_declaration_ast_node(c
     : type_declaration_ast_node(name)
 {
 }
+
+codesh::ast::type::custom_type_ast_node *codesh::ast::type_decl::class_declaration_ast_node::
+    get_super_class() const
+{
+    return super_class.get();
+}
+
+void codesh::ast::type_decl::class_declaration_ast_node::set_super_class(
+    std::unique_ptr<type::custom_type_ast_node> super_class)
+{
+    this->super_class = std::move(super_class);
+}
