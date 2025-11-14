9811471d40736d4539143f3713c6c7ccb3bde583
Elione <eliranbmo2008@gmail.com>
2025-11-15 01:01:25 +0200

    finish building the name handling and words coming after KEYWORD_LET

--- src/parser/type/class_parser.cpp
+++ src/parser/type/class_parser.cpp
@@ -11,8 +11,7 @@ namespace parser = codesh::parser;
 static void parse_class_scope(std::queue<std::unique_ptr<codesh::token>> &tokens,
         ast::type_decl::class_declaration_ast_node *class_node);
 
-static void parse_method_scope(std::queue<std::unique_ptr<codesh::token>> &tokens,
-        ast::method_declaration_ast_node *method_node);
+static void parse_method_scope(std::queue<std::unique_ptr<codesh::token>> &tokens);
 
 
 std::unique_ptr<ast::type_decl::class_declaration_ast_node> codesh::parser::parse_class_declaration(
@@ -63,31 +62,54 @@ static void parse_class_scope(std::queue<std::unique_ptr<codesh::token>> &tokens
         switch (parser::util::consume_token(tokens)->get_group())
         {
         case codesh::token_group::KEYWORD_LET:
-            ast::method_declaration_ast_node *method_node; // TODO: initialize
-            parse_method_scope(tokens, method_node);
+            {
+            codesh::token_group kind = tokens.front()->get_group();
+            if (kind == codesh::token_group::KEYWORD_FUNCTION)
+            {
+                parse_method_scope(tokens);
+            }
+            else if (kind == codesh::token_group::KEYWORD_CLASS)
+            {
+                // TODO: Add ויהי עצם
+                continue;
+            }
+            else if (kind == codesh::token_group::KEYWORD_VAR)
+            {
+                //TODO: Add ויהי משתנה
+                continue;
+            }
+            else
+            {
+                throw std::runtime_error("Unexpected token"); //TODO: Add custom codesh error messege
+            }
             break;
-
+            }
         case codesh::token_group::SCOPE_END: return;
         default: throw std::runtime_error("Unexpected token");
         }
     }
-
     throw std::runtime_error("Unexpected EOF: Expected end of scope (ויתם:)");
 }
 
-static void parse_method_scope(std::queue<std::unique_ptr<codesh::token>> &tokens,
-        ast::method_declaration_ast_node *method_node)
+static void parse_method_scope(std::queue<std::unique_ptr<codesh::token>> &tokens)
 {
     while (!tokens.empty())
     {
-        if (parser::util::consume_token(tokens)->get_group() != codesh::token_group::KEYWORD_FUNCTION) // TODO: it can be ויהי עצם another time or ויהי משתנה
-        {
-            throw std::runtime_error("Unexpected token: Expected מעשה");
-        }
-
+        // ושמו
         if (parser::util::consume_token(tokens)->get_group() != codesh::token_group::KEYWORD_NAME)
         {
             throw std::runtime_error("Unexpected token: Expected ושמו");
         }
+
+
+        const std::unique_ptr<codesh::token> name_token = parser::util::consume_token(tokens);
+        if (name_token->get_group() != codesh::token_group::IDENTIFIER)
+        {
+            throw std::runtime_error("Unexpected token: Expected identifier");
+        }
+
+        ast::method_declaration_ast_node method_node;
+        method_node.set_name(dynamic_cast<const codesh::identifier_token *>(name_token.get())->get_content());
+
     }
 }
