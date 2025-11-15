ab2a63f227ec499dfd78971fafb7d8b5d7872999
StavWasPlayZ <stavsolomon101@gmail.com>
2025-11-15 02:42:18 +0200

    Use unordered set instead of regular set in constant_pool_builder.cpp

--- src/output/jvm_target/constant_pool_builder.cpp
+++ src/output/jvm_target/constant_pool_builder.cpp
@@ -1,19 +1,20 @@
 #include "constant_pool_builder.h"
 
-#include <set>
+#include <unordered_set>
 
 #include "../../parser/ast/compilation_unit_ast_node.h"
 #include "../../parser/ast/type_declaration/class_declaration_ast_node.h"
 
-static void traverse_type_decls(const codesh::ast::compilation_unit_ast_node &root_node, std::set<std::string> &literals);
+static void traverse_type_decls(const codesh::ast::compilation_unit_ast_node &root_node, std::unordered_set<std::string> &literals);
 static void traverse_class_decl(const codesh::ast::type_decl::class_declaration_ast_node &class_decl_node,
-        std::set<std::string> &literals);
+        std::unordered_set<std::string> &literals);
 
 
+//TODO: Support other non-string literals
 std::unordered_map<std::string, int> codesh::output::jvm_target::constant_pool_builder::build(
     const ast::compilation_unit_ast_node &root_node)
 {
-    std::set<std::string> literals = {
+    std::unordered_set<std::string> literals = {
         "SourceFile",
         root_node.get_source_stem() + ".אמן"
     };
@@ -43,7 +44,7 @@ std::unordered_map<std::string, int> codesh::output::jvm_target::constant_pool_b
     return results;
 }
 
-static void traverse_type_decls(const codesh::ast::compilation_unit_ast_node &root_node, std::set<std::string> &literals)
+static void traverse_type_decls(const codesh::ast::compilation_unit_ast_node &root_node, std::unordered_set<std::string> &literals)
 {
     for (const auto &type_decl : root_node.get_type_declarations())
     {
@@ -58,7 +59,7 @@ static void traverse_type_decls(const codesh::ast::compilation_unit_ast_node &ro
 }
 
 static void traverse_class_decl(const codesh::ast::type_decl::class_declaration_ast_node &class_decl_node,
-        std::set<std::string> &literals)
+        std::unordered_set<std::string> &literals)
 {
     const auto super_class = class_decl_node.get_super_class();
 
