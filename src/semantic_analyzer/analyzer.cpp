9038a91c20daec57e22f078e3378cedad90b81ea
Elione <eliranbmo2008@gmail.com>
2025-11-19 18:12:45 +0200

    build main files and simple run function

---
+++ src/semantic_analyzer/analyzer.cpp
@@ -0,0 +1,11 @@
+#include "analyzer.h"
+
+namespace codesh::semantic
+{
+void run(ast::compilation_unit_ast_node& root)
+{
+    // build_symbol_table(root);
+    // check_types(root);
+    // verify_methods(root);
+}
+}
--- src/semantic_analyzer/analyzer.h
+++ src/semantic_analyzer/analyzer.h
@@ -1,1 +1,11 @@
 #pragma once
+
+#include "../parser/ast/compilation_unit_ast_node.h"
+
+
+namespace codesh::parser
+{
+
+    static void run(ast::compilation_unit_ast_node & root);
+
+}
