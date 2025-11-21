7c3e548d704112c290ef5114d6290bcfa308da9e
StavWasPlayZ <stavsolomon101@gmail.com>
2025-11-21 22:38:20 +0200

    Implement symbol table

--- src/semantic_analyzer/symbol_table/symbol_table.cpp
+++ src/semantic_analyzer/symbol_table/symbol_table.cpp
@@ -1,6 +1,33 @@
 #include "symbol_table.h"
 
-namespace codesh::semantic_analyzer
+std::vector<codesh::semantic_analyzer::symbol_type> codesh::semantic_analyzer::symbol_table::allowed_symbol_types()
+    const
 {
-
+    return ALLOWED_SYMBOL_TYPES;
 }
+
+const codesh::semantic_analyzer::named_scope_map &codesh::semantic_analyzer::symbol_table::get_symbol_map() const
+{
+    return global_scope;
+}
+
+codesh::semantic_analyzer::named_scope_map &codesh::semantic_analyzer::symbol_table::get_symbol_map()
+{
+    return global_scope;
+}
+
+std::optional<std::reference_wrapper<codesh::semantic_analyzer::package_symbol>> codesh::semantic_analyzer::
+    symbol_table::resolve_optional(const std::string &name) const
+{
+    const auto result = resolve(name);
+
+    if (!result)
+        return std::nullopt;
+
+    // Only packages allowed anyway
+    return *static_cast<package_symbol *>(&result.value().get()); // NOLINT(*-pro-type-static-cast-downcast)
+}
+
+const std::vector<codesh::semantic_analyzer::symbol_type> codesh::semantic_analyzer::symbol_table::ALLOWED_SYMBOL_TYPES = {
+    symbol_type::PACKAGE
+};
--- src/semantic_analyzer/symbol_table/symbol_table.h
+++ src/semantic_analyzer/symbol_table/symbol_table.h
@@ -5,9 +5,19 @@
 namespace codesh::semantic_analyzer
 {
 
-class symbol_table
+class symbol_table final : public i_scope_containing_symbol
 {
-    std::unordered_map<std::string, package_symbol> global_scope;
+    static const std::vector<symbol_type> ALLOWED_SYMBOL_TYPES;
+    named_scope_map global_scope;
+
+protected:
+    [[nodiscard]] std::vector<symbol_type> allowed_symbol_types() const override;
+
+    [[nodiscard]] const named_scope_map &get_symbol_map() const override;
+    [[nodiscard]] named_scope_map &get_symbol_map() override;
+
+public:
+    [[nodiscard]] std::optional<std::reference_wrapper<package_symbol>> resolve_optional(const std::string &name) const;
 };
 
 }
