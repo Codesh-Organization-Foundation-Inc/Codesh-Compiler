a1383eed466fefadc12f6c7cbb3edfc5608941dc
Elione <eliranbmo2008@gmail.com>
2025-12-01 18:59:06 +0200

    made the base of semantic error collection

--- src/semantic_analyzer/errors/errors.cpp
+++ src/semantic_analyzer/errors/errors.cpp
@@ -3,7 +3,23 @@
 #include <stdexcept>
 #include <string>
 
-void codesh::semantic_analyzer::throw_error(const std::string &msg)
+namespace codesh::errors
 {
-    throw std::runtime_error("semantic error: " + msg);
+
+
+void add_error(const std::string& msg)
+{
+    errors_vector.push_back({ msg, 0, 0});
 }
+
+void add_error(const std::string& msg, int line, int column)
+{
+    errors_vector.push_back({ msg, line, column});
+    //TODO: add the line and column
+}
+
+}
+// void codesh::semantic_analyzer::throw_error(const std::string &msg)
+// {
+//     throw std::runtime_error("semantic error: " + msg);
+// }
--- src/semantic_analyzer/errors/errors.h
+++ src/semantic_analyzer/errors/errors.h
@@ -1,8 +1,40 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 namespace codesh::semantic_analyzer
 {
-void throw_error(const std::string &msg);
+
+struct compiler_error
+{
+    std::string message;
+    int line;
+    int column;
+};
+
+class error_collection
+{
+public:
+
+    // Add a new error
+    void add_error(const std::string& msg);
+
+    void add_error(const std::string& msg, int line, int column);
+
+    // Whether any errors exist
+    [[nodiscard]] bool has_errors() const;
+
+    // Print all errors
+    void print_errors() const;
+
+    // Return const reference to all errors
+    [[nodiscard]] const std::vector<compiler_error>& get_errors() const { return errors; }
+
+private:
+    std::vector<compiler_error>errors_vector;
+
+};
+
 }
+
