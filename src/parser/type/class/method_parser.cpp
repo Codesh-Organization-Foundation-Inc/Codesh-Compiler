a164509c9a3eec7c4c2b55c227a0d84d62352f40
StavWasPlayZ <stavsolomon101@gmail.com>
2025-12-08 00:50:12 +0200

    Refactor undry code

--- src/parser/type/class/method_parser.cpp
+++ src/parser/type/class/method_parser.cpp
@@ -6,8 +6,27 @@
 #include "../../ast/var_reference/evaluable_ast_node.h"
 #include "../../util.h"
 
+#include <functional>
+
 static void parse_methods_call_parameters(std::queue<std::unique_ptr<codesh::token>> &tokens,
         codesh::ast::method::operation::method_call_ast_node &method_call);
+
+template <typename T>
+static std::unique_ptr<codesh::ast::var_reference::evaluable_ast_node<T>> make_evaluable(
+        std::queue<std::unique_ptr<codesh::token>> &tokens,
+        codesh::definition::primitive_type primitive_type,
+        const std::function<T(const std::string &)> &mapper);
+
+template <typename T>
+static std::unique_ptr<codesh::ast::var_reference::evaluable_ast_node<T>> make_evaluable(
+        std::queue<std::unique_ptr<codesh::token>> &tokens,
+        codesh::definition::primitive_type primitive_type,
+        T value);
+
+static std::unique_ptr<codesh::ast::var_reference::evaluable_ast_node<bool>> make_bool_evaluable(
+        std::queue<std::unique_ptr<codesh::token>> &tokens,
+        bool value);
+
 
 void codesh::parser::parse_method(std::queue<std::unique_ptr<token>> &tokens)
 {
@@ -62,60 +81,65 @@ static void parse_methods_call_parameters(std::queue<std::unique_ptr<codesh::tok
             break;
         }
         case codesh::token_group::LITERAL_NUMBER_INT: {
-            eval_ast_node = std::make_unique<codesh::ast::var_reference::evaluable_ast_node<int>>(
-                std::make_unique<codesh::ast::type::primitive_type_ast_node>(
-                    codesh::definition::primitive_type::INTEGER
-                ),
-                std::stoi(codesh::parser::util::consume_alnum_identifier_token(tokens)->get_content())
+            eval_ast_node = make_evaluable<int>(
+                tokens,
+                codesh::definition::primitive_type::INTEGER,
+
+                [](const std::string &content) {
+                    return std::stoi(content);
+                }
             );
 
             break;
         }
         case codesh::token_group::LITERAL_NUMBER_FLOAT: {
-            eval_ast_node = std::make_unique<codesh::ast::var_reference::evaluable_ast_node<float>>(
-                std::make_unique<codesh::ast::type::primitive_type_ast_node>(
-                    codesh::definition::primitive_type::FLOAT
-                ),
-                std::stof(codesh::parser::util::consume_alnum_identifier_token(tokens)->get_content())
+            eval_ast_node = make_evaluable<float>(
+                tokens,
+                codesh::definition::primitive_type::FLOAT,
+
+                [](const std::string &content) {
+                    return std::stof(content);
+                }
             );
 
             break;
         }
         case codesh::token_group::LITERAL_NUMBER_DOUBLE: {
-            eval_ast_node = std::make_unique<codesh::ast::var_reference::evaluable_ast_node<double>>(
-                std::make_unique<codesh::ast::type::primitive_type_ast_node>(
-                    codesh::definition::primitive_type::DOUBLE
-                ),
-                std::stod(codesh::parser::util::consume_alnum_identifier_token(tokens)->get_content())
+            eval_ast_node = make_evaluable<double>(
+                tokens,
+                codesh::definition::primitive_type::DOUBLE,
+
+                [](const std::string &content) {
+                    return std::stod(content);
+                }
             );
 
             break;
         }
         case codesh::token_group::LITERAL_CHAR: {
-            eval_ast_node = std::make_unique<codesh::ast::var_reference::evaluable_ast_node<char>>(
-                std::make_unique<codesh::ast::type::primitive_type_ast_node>(
-                    codesh::definition::primitive_type::CHAR
-                ),
-                codesh::parser::util::consume_alnum_identifier_token(tokens)->get_content()[0]
+            eval_ast_node = make_evaluable<char>(
+                tokens,
+                codesh::definition::primitive_type::CHAR,
+
+                [](const std::string &content) {
+                    return content[0];
+                }
             );
 
             break;
         }
+
         case codesh::token_group::KEYWORD_TRUE: {
-            eval_ast_node = std::make_unique<codesh::ast::var_reference::evaluable_ast_node<bool>>(
-                std::make_unique<codesh::ast::type::primitive_type_ast_node>(
-                    codesh::definition::primitive_type::BOOLEAN
-                ),
+            eval_ast_node = make_bool_evaluable(
+                tokens,
                 true
             );
 
             break;
         }
         case codesh::token_group::KEYWORD_FALSE: {
-            eval_ast_node = std::make_unique<codesh::ast::var_reference::evaluable_ast_node<bool>>(
-                std::make_unique<codesh::ast::type::primitive_type_ast_node>(
-                    codesh::definition::primitive_type::BOOLEAN
-                ),
+            eval_ast_node = make_bool_evaluable(
+                tokens,
                 false
             );
 
@@ -123,23 +147,47 @@ static void parse_methods_call_parameters(std::queue<std::unique_ptr<codesh::tok
         }
 
         }
-        // if (tokens.front()->get_group() == codesh::token_group::IDENTIFIER)
-        // {
-        // }
-        // else if (tokens.front()->get_group() == codesh::token_group::LITERAL_NUMBER_INT)
-        // {
-        //
-        // }
-        // else if (tokens.front()->get_group() == codesh::token_group::LITERAL_NUMBER_FLOAT)
-        // {
-        //
-        // }
-        // else if (tokens.front()->get_group() == codesh::token_group::LITERAL_NUMBER_DOUBLE)
-        // {
-        //
-        // }
 
         method_call.get_arguments().push_back(std::move(eval_ast_node));
     }
 
+}
+
+template <typename T>
+static std::unique_ptr<codesh::ast::var_reference::evaluable_ast_node<T>> make_evaluable(
+        std::queue<std::unique_ptr<codesh::token>> &tokens,
+        codesh::definition::primitive_type primitive_type,
+        const std::function<T(const std::string &)> &mapper)
+{
+    return std::make_unique<codesh::ast::var_reference::evaluable_ast_node<T>>(
+        std::make_unique<codesh::ast::type::primitive_type_ast_node>(primitive_type),
+        mapper(codesh::parser::util::consume_alnum_identifier_token(tokens)->get_content())
+    );
+}
+
+template <typename T>
+static std::unique_ptr<codesh::ast::var_reference::evaluable_ast_node<T>> make_evaluable(
+        std::queue<std::unique_ptr<codesh::token>> &tokens,
+        codesh::definition::primitive_type primitive_type,
+        T value)
+{
+    tokens.pop();
+
+    return std::make_unique<codesh::ast::var_reference::evaluable_ast_node<T>>(
+        std::make_unique<codesh::ast::type::primitive_type_ast_node>(primitive_type),
+        std::move(value)
+    );
+}
+
+static std::unique_ptr<codesh::ast::var_reference::evaluable_ast_node<bool>> make_bool_evaluable(
+        std::queue<std::unique_ptr<codesh::token>> &tokens,
+        const bool value)
+{
+    tokens.pop();
+
+    return make_evaluable<bool>(
+        tokens,
+        codesh::definition::primitive_type::BOOLEAN,
+        value
+    );
 }
