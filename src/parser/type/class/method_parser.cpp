4318aa2c77365af59bbf698686b6be2d525ea912
StavWasPlayZ <stavsolomon101@gmail.com>
2025-12-08 16:02:05 +0200

    Throw syntax error on tokens ran out while parsing method

--- src/parser/type/class/method_parser.cpp
+++ src/parser/type/class/method_parser.cpp
@@ -1,5 +1,6 @@
 #include "method_parser.h"
 
+#include "../../../blasphemies/blasphemy_collector.h"
 #include "../../util.h"
 
 void codesh::parser::parse_method(std::queue<std::unique_ptr<token>> &tokens)
@@ -16,4 +17,6 @@ void codesh::parser::parse_method(std::queue<std::unique_ptr<token>> &tokens)
         default: throw std::runtime_error("Unexpected token");
         }
     }
+
+    error::get_blasphemy_collector().add_blasphemy("נָבוֹא שְׁקָרַי: צִפָּה לְ־וַיַתָם", error::blasphemy_type::SYNTAX);
 }
