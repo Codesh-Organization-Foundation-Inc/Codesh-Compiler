c07f170fe75f70a16a088fa1a6769de289d8d276
StavWasPlayZ <stavsolomon101@gmail.com>
2025-12-10 07:32:46 +0200

    Implement symbol table structure

---
+++ src/output/local_variable_table.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <vector>
+
+namespace codesh::output
+{
+
+struct local_variable_table_entry
+{
+    size_t start;
+    size_t length;
+    std::string name;
+    std::string signature;
+};
+
+class local_variable_table
+{
+    std::vector<local_variable_table_entry> entries;
+
+public:
+    [[nodiscard]] const std::vector<local_variable_table_entry> &get_entries() const;
+
+    void add_entry(local_variable_table_entry entry);
+    [[nodiscard]] const local_variable_table_entry &get_entry(size_t index) const;
+};
+
+}
