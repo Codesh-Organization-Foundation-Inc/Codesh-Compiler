840ba23ee4a8b5f0c1287f88d8d46bed8c352cae
StavWasPlayZ <stavsolomon101@gmail.com>
2025-12-11 17:12:12 +0200

    Reduce boilerplate code using new symbol reference in method AST node

--- src/semantic_analyzer/method_decl/resolve.cpp
+++ src/semantic_analyzer/method_decl/resolve.cpp
@@ -11,22 +11,18 @@
 #include <ranges>
 
 static void resolve_return_type(const codesh::ast::compilation_unit_ast_node &root,
-        const codesh::ast::method::method_declaration_ast_node &method_decl,
-        const codesh::semantic_analyzer::method_symbol &method_symbol);
+        const codesh::ast::method::method_declaration_ast_node &method_decl);
 
 static void resolve_parameters(const codesh::ast::compilation_unit_ast_node &root,
-        const codesh::ast::method::method_declaration_ast_node &method,
-        const codesh::semantic_analyzer::method_symbol &method_symbol);
+        const codesh::ast::method::method_declaration_ast_node &method_decl);
 
 static void resolve_local_variables(const codesh::ast::compilation_unit_ast_node &root,
-        const codesh::ast::method::method_declaration_ast_node &method_decl,
-        const codesh::semantic_analyzer::method_symbol &method_symbol);
+        const codesh::ast::method::method_declaration_ast_node &method_decl);
 
 
 static const std::function<
         void(const codesh::ast::compilation_unit_ast_node &root,
-             const codesh::ast::method::method_declaration_ast_node &method_decl,
-             const codesh::semantic_analyzer::method_symbol &method_symbol)
+             const codesh::ast::method::method_declaration_ast_node &method_decl)
     > METHOD_RESOLVERS[] = {
     resolve_return_type,
     resolve_parameters,
@@ -35,38 +31,23 @@ static const std::function<
 
 
 void codesh::semantic_analyzer::method_declaration::resolve_methods(const ast::compilation_unit_ast_node &root) {
-    //TODO: Properly get country of origin
-    const country_symbol &country = root.get_symbol_table()->get().resolve_country("").value();
-
     for (const auto &type_decl : root.get_type_declarations())
     {
         const auto *class_node = dynamic_cast<ast::type_decl::class_declaration_ast_node *>(type_decl.get());
         if (!class_node)
             continue;
 
-        // Get type symbol
-        const type_symbol &type = *static_cast<type_symbol *>(&country.resolve(type_decl->get_name()).value().get()); // NOLINT(*-pro-type-static-cast-downcast)
-
         for (const auto &method_decl : class_node->get_all_methods())
         {
-            method_overloads_symbol &method_overloads = *static_cast<method_overloads_symbol *>( // NOLINT(*-pro-type-static-cast-downcast)
-                &type.resolve(method_decl->get_name()).value().get()
-            );
-
-            // Get relevant method symbol from the method overloads map
-            // Then cast it to method_symbol
-            std::unique_ptr<method_symbol> method(
-                static_cast<method_symbol *>( // NOLINT(*-pro-type-static-cast-downcast)
-                    method_overloads.resolve_and_move(method_decl->generate_parameters_descriptor(false))
-                        .release()
-                )
-            );
+            // Save the original descriptor.
+            // This will be changed when applying the resolvers.
+            const std::string original_descriptor = method_decl->generate_parameters_descriptor(false);
 
             for (const auto &resolver : METHOD_RESOLVERS)
             {
                 try
                 {
-                    resolver(root, *method_decl, *method);
+                    resolver(root, *method_decl);
                 }
                 catch (const std::runtime_error &e)
                 {
@@ -80,8 +61,21 @@ void codesh::semantic_analyzer::method_declaration::resolve_methods(const ast::c
                 }
             }
 
-            // Move to a new overloads entry now that the parameters' descriptors are real
-            method_overloads.add_symbol(method_decl->generate_parameters_descriptor(), std::move(method));
+
+            // Move to a new overloads entry now that the parameters' descriptors are real:
+            const auto &method_overloads = static_cast<method_overloads_symbol *>( // NOLINT(*-pro-type-static-cast-downcast)
+                method_decl->get_symbol().get_parent_symbol()
+            );
+
+            // Get relevant method symbol from the method overloads map
+            // Then cast it to method_symbol
+            std::unique_ptr<method_symbol> method(
+                static_cast<method_symbol *>( // NOLINT(*-pro-type-static-cast-downcast)
+                    method_overloads->resolve_and_move(original_descriptor).release()
+                )
+            );
+
+            method_overloads->add_symbol(method_decl->generate_parameters_descriptor(), std::move(method));
         }
 
     }
@@ -89,8 +83,7 @@ void codesh::semantic_analyzer::method_declaration::resolve_methods(const ast::c
 
 
 static void resolve_return_type(const codesh::ast::compilation_unit_ast_node &root,
-        const codesh::ast::method::method_declaration_ast_node &method_decl,
-        const codesh::semantic_analyzer::method_symbol &method_symbol)
+        const codesh::ast::method::method_declaration_ast_node &method_decl)
 {
     auto *return_type = dynamic_cast<codesh::ast::type::custom_type_ast_node *>(method_decl.get_return_type());
 
@@ -108,15 +101,14 @@ static void resolve_return_type(const codesh::ast::compilation_unit_ast_node &ro
     if (!codesh::semantic_analyzer::util::resolve_custom_type_node(
         lookup_countries,
         *return_type,
-        method_symbol.get_return_type()
+        method_decl.get_symbol().get_return_type()
     )) {
         throw std::runtime_error("Unknown return type " + return_type->get_name());
     }
 }
 
 static void resolve_parameters(const codesh::ast::compilation_unit_ast_node &root,
-        const codesh::ast::method::method_declaration_ast_node &method,
-        const codesh::semantic_analyzer::method_symbol &method_symbol)
+        const codesh::ast::method::method_declaration_ast_node &method_decl)
 {
     //TODO: Use actual countries
     const std::vector lookup_countries = {
@@ -124,7 +116,7 @@ static void resolve_parameters(const codesh::ast::compilation_unit_ast_node &roo
     };
 
     size_t i = 0;
-    for (const auto &param : method.get_parameters())
+    for (const auto &param : method_decl.get_parameters())
     {
         auto *custom_param = dynamic_cast<codesh::ast::type::custom_type_ast_node *>(param->get_type());
         if (!custom_param)
@@ -133,7 +125,7 @@ static void resolve_parameters(const codesh::ast::compilation_unit_ast_node &roo
         if (!codesh::semantic_analyzer::util::resolve_custom_type_node(
             lookup_countries,
             *custom_param,
-            *method_symbol.get_parameter_types()[i]
+            *method_decl.get_symbol().get_parameter_types()[i]
         )) {
             throw std::runtime_error("Unknown return type " + custom_param->get_name());
         }
@@ -143,15 +135,14 @@ static void resolve_parameters(const codesh::ast::compilation_unit_ast_node &roo
 }
 
 static void resolve_local_variables(const codesh::ast::compilation_unit_ast_node &root,
-        const codesh::ast::method::method_declaration_ast_node &,
-        const codesh::semantic_analyzer::method_symbol &method_symbol)
+        const codesh::ast::method::method_declaration_ast_node &method_decl)
 {
     //TODO: Use actual countries
     const std::vector lookup_countries = {
         root.get_symbol_table().value().get().resolve_country("").value()
     };
 
-    for (const auto &var_symbol : method_symbol.get_scope().get_variables() | std::views::values)
+    for (const auto &var_symbol : method_decl.get_symbol().get_scope().get_variables() | std::views::values)
     {
         auto *custom_param = dynamic_cast<codesh::ast::type::custom_type_ast_node *>(var_symbol->get_type());
         if (!custom_param)
@@ -162,6 +153,6 @@ static void resolve_local_variables(const codesh::ast::compilation_unit_ast_node
             *custom_param
         )) {
             throw std::runtime_error("Unknown type " + custom_param->get_name());
-        };
+        }
     }
 }
