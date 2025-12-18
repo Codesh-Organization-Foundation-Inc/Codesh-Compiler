f6b8a6553c34549e87d2b24cd2bd5677913f5cec
StavWasPlayZ <stavsolomon101@gmail.com>
2025-12-18 10:05:36 +0200

    Revert "Revert method calls issue"
    This reverts commit c30a5ebe9942c7ad475ac90ab2902213883bc749.

--- src/semantic_analyzer/util.h
+++ src/semantic_analyzer/util.h
@@ -32,6 +32,14 @@ namespace codesh::semantic_analyzer::util
         const definition::fully_qualified_class_name &fqcn);
 
 /**
+ * @returns the Fully Qualified Class Name of the given type name (which isn't a descriptor).
+ * If unresolved, returns {@link std::nullopt}
+ */
+[[nodiscard]] std::optional<definition::fully_qualified_class_name> resolve_method_call(
+        const std::vector<std::reference_wrapper<country_symbol>> &lookup_countries,
+        const definition::fully_qualified_class_name &fqcn);
+
+/**
  * @return Whether the type was successfully resolved
  */
 bool resolve_custom_type_node(const std::vector<std::reference_wrapper<country_symbol>> &lookup_countries,
