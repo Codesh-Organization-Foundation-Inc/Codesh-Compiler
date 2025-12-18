e2402537ca8ec8c09d33aa26ae76d6ac6dcbc6ad
StavWasPlayZ <stavsolomon101@gmail.com>
2025-12-18 18:56:05 +0200

    Call method call resolver when resolving method declarations

--- src/semantic_analyzer/method_decl/resolve.cpp
+++ src/semantic_analyzer/method_decl/resolve.cpp
@@ -1,11 +1,13 @@
 #include "resolve.h"
+#include "../method_call/resolve.h"
 
 #include "../../blasphemy/blasphemy_collector.h"
 #include "../../parser/ast/compilation_unit_ast_node.h"
 #include "../../parser/ast/type/custom_type_ast_node.h"
+#include "../../parser/ast/method/operation/method_call_ast_node.h"
 #include "../../parser/ast/type/primitive_type_ast_node.h"
-#include "../util.h"
 #include "../semantic_context.h"
+#include "../util.h"
 
 #include <functional>
 #include <ranges>
@@ -21,21 +23,32 @@ static void resolve_parameters(const codesh::semantic_analyzer::semantic_context
 static void resolve_local_variables(const codesh::semantic_analyzer::semantic_context &context,
                                     const codesh::semantic_analyzer::method_symbol &method_symbol);
 
-static void resolve_method_signature(const codesh::semantic_analyzer::semantic_context &context,
-                                     const codesh::ast::method::method_declaration_ast_node &method_decl,
-                                     const codesh::semantic_analyzer::type_symbol &type);
+static codesh::semantic_analyzer::method_symbol &resolve_method_signature(
+        const codesh::semantic_analyzer::semantic_context &context,
+        const codesh::ast::method::method_declaration_ast_node &method_decl,
+        const codesh::semantic_analyzer::type_symbol &type);
 
 
 void codesh::semantic_analyzer::method_declaration::resolve(
     const semantic_context &context, const type_symbol &type,
     const ast::method::method_declaration_ast_node &method_decl)
 {
-    resolve_method_signature(context, method_decl, type);
+    const auto &method = resolve_method_signature(context, method_decl, type);
+
+    // Resolve body statements
+    for (const auto &statement : method_decl.get_body())
+    {
+        if (const auto method_call = dynamic_cast<ast::method::operation::method_call_ast_node *>(statement.get()))
+        {
+            method_call::resolve(context, *method_call, method);
+        }
+    }
 }
 
-static void resolve_method_signature(const codesh::semantic_analyzer::semantic_context &context,
-                                     const codesh::ast::method::method_declaration_ast_node &method_decl,
-                                     const codesh::semantic_analyzer::type_symbol &type)
+static codesh::semantic_analyzer::method_symbol &resolve_method_signature(
+        const codesh::semantic_analyzer::semantic_context &context,
+        const codesh::ast::method::method_declaration_ast_node &method_decl,
+        const codesh::semantic_analyzer::type_symbol &type)
 {
     const std::string name = method_decl.get_name();
     const auto new_context = context.with_consumer("בְּמַעֲשֶׂה", name);
@@ -58,8 +71,12 @@ static void resolve_method_signature(const codesh::semantic_analyzer::semantic_c
     resolve_parameters(new_context, method_decl, *method);
     resolve_local_variables(new_context, *method);
 
+
     // Move to a new overloads entry, now that the parameters' descriptors are real
-    method_overloads.add_symbol(method_decl.generate_parameters_descriptor(), std::move(method));
+    const auto insert_result =
+        method_overloads.add_symbol(method_decl.generate_parameters_descriptor(), std::move(method));
+
+    return insert_result.first.get();
 }
 
 static void resolve_return_type(const codesh::semantic_analyzer::semantic_context &context,
