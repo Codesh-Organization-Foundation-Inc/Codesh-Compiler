e37e48acf7aee9cf8907f8077298c3d763c04b09
Elione <eliranbmo2008@gmail.com>
2025-12-24 22:30:49 +0200

    fixed last commit

--- src/parser/ast/impl/binary_ast_node.h
+++ src/parser/ast/impl/binary_ast_node.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../var_reference/value_ast_node.h"
+#include "ir_emitting_ast_node.h"
 
 #include <memory>
 
@@ -8,17 +9,17 @@ namespace codesh::ast::impl
 
 class binary_ast_node : public var_reference::value_ast_node
 {
-    std::unique_ptr<value_ast_node> left, right;
+    std::unique_ptr<ir_emitting_ast_node> left, right;
 
 protected:
-    binary_ast_node(std::unique_ptr<value_ast_node> left, std::unique_ptr<value_ast_node> right);
+    binary_ast_node(std::unique_ptr<ir_emitting_ast_node> left, std::unique_ptr<ir_emitting_ast_node> right);
 
 public:
-    [[nodiscard]] value_ast_node *get_left() const;
-    [[nodiscard]] value_ast_node *get_right() const;
+    [[nodiscard]] ir_emitting_ast_node *get_left() const;
+    [[nodiscard]] ir_emitting_ast_node *get_right() const;
 
-    void set_left(std::unique_ptr<value_ast_node> node);
-    void set_right(std::unique_ptr<value_ast_node> node);
+    void set_left(std::unique_ptr<ir_emitting_ast_node> node);
+    void set_right(std::unique_ptr<ir_emitting_ast_node> node);
 };
 
 
