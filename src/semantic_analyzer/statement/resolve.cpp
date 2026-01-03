7e4d7908c8e807e2ad5909f24bc365999726efe6
StavWasPlayZ <stavsolomon101@gmail.com>
2026-01-03 18:36:42 +0200

    Make method arguments' resolver refer to statement resolver

--- src/semantic_analyzer/statement/resolve.cpp
+++ src/semantic_analyzer/statement/resolve.cpp
@@ -1,15 +1,23 @@
 #include "resolve.h"
 #include "method_call/resolve.h"
+#include "variable_reference/resolve.h"
 
+#include "../../parser/ast/var_reference/variable_reference_ast_node.h"
 #include "../../semantic_analyzer/symbol_table/symbol.h"
 
-void codesh::semantic_analyzer::statement::resolve(const semantic_context &context,
+bool codesh::semantic_analyzer::statement::resolve(const semantic_context &context,
                                                    ast::method::operation::method_operation_ast_node &stmnt,
                                                    const method_symbol &containing_method,
                                                    const method_scope_symbol &scope)
 {
     if (const auto method_call = dynamic_cast<ast::method::operation::method_call_ast_node *>(&stmnt))
     {
-        method_call::resolve(context, *method_call, containing_method, scope);
+        return method_call::resolve(context, *method_call, containing_method, scope);
     }
+    if (const auto var_ref = dynamic_cast<variable_reference_ast_node *>(&stmnt))
+    {
+        return variable_reference::resolve(context, *var_ref, scope);
+    }
+
+    throw std::runtime_error("Unsupported statement");
 }
