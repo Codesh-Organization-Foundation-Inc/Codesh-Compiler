4360ccc052c70328dc32707ab72c11ad00b866e6
Elione <eliranbmo2008@gmail.com>
2026-01-05 13:07:52 +0200

    made the base of functions in assignment classes

--- src/parser/ast/operator/assignment/subtraction_assignment_operator_ast_node.h
+++ src/parser/ast/operator/assignment/subtraction_assignment_operator_ast_node.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "assignment_operator_ast_node.h"
+
+namespace codesh::ast::op::assignment
+{
+class subtraction_assignment_operator_ast_node final : public assignment_operator_ast_node
+{
+    subtraction_assignment_operator_ast_node(std::unique_ptr<variable_reference_ast_node> left, std::unique_ptr<value_ast_node> right);
+
+    void emit_ir(output::ir::code_block &containing_block, const semantic_analyzer::symbol_table &symbol_table,
+                 const type_decl::type_declaration_ast_node &containing_type_decl) const override;
+};
+}
