c85433b787a245dd4d950552688a03ac9252bee9
Elione <eliranbmo2008@gmail.com>
2026-01-10 20:05:41 +0200

    finished making the if_ast_node.h

--- src/parser/ast/method/operation/block/if_ast_node.h
+++ src/parser/ast/method/operation/block/if_ast_node.h
@@ -1,6 +1,13 @@
 #pragma once
 #include "../method_operation_ast_node.h"
 
+#include <memory>
+#include <optional>
+
+namespace codesh::ast::var_reference
+{
+class value_ast_node;
+}
 namespace codesh::ast::method
 {
 class method_scope_ast_node;
@@ -10,7 +17,10 @@ namespace codesh::ast::block // TODO: do it like - block::logic
 
 class if_ast_node : public method::operation::method_operation_ast_node
 {
-    method::method_scope_ast_node &method_scopes;
+    std::unique_ptr<var_reference::value_ast_node> condition;
+    method::method_scope_ast_node &if_scope;
+    std::optional<method::method_scope_ast_node> &else_scope;
+
 
 public:
     explicit if_ast_node(method::method_scope_ast_node method_scope);
