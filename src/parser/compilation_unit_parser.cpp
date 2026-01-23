c0210a5433076cdfc3e4b8d8113cf363dd2373ba
StavWasPlayZ <stavsolomon101@gmail.com>
2026-01-23 21:44:46 +0200

    Remove useless function

--- src/parser/compilation_unit_parser.cpp
+++ src/parser/compilation_unit_parser.cpp
@@ -7,11 +7,6 @@
 #include "util.h"
 
 namespace ast = codesh::ast;
-namespace parser = codesh::parser;
-
-static void parse_origin_country(std::queue<std::unique_ptr<codesh::token>> &tokens,
-                                 ast::compilation_unit_ast_node *root_node);
-
 
 std::unique_ptr<ast::compilation_unit_ast_node> codesh::parser::parse_compilation_unit(
         std::queue<std::unique_ptr<token>> &tokens, const std::string &source_stem)
@@ -24,17 +19,11 @@ std::unique_ptr<ast::compilation_unit_ast_node> codesh::parser::parse_compilatio
     {
         if (tokens.front()->get_group() == token_group::KEYWORD_ORIGIN_COUNTRY)
         {
-            parse_origin_country(tokens, node.get());
+            tokens.pop();
+            util::parse_fqcn(tokens, node->get_package_name());
+            util::ensure_end_op(tokens);
         }
     }
 
     return node;
-}
-
-static void parse_origin_country(std::queue<std::unique_ptr<codesh::token>> &tokens,
-                                 ast::compilation_unit_ast_node *root_node)
-{
-    tokens.pop();
-    parser::util::parse_fqcn(tokens, root_node->get_package_name());
-    parser::util::ensure_end_op(tokens);
 }
