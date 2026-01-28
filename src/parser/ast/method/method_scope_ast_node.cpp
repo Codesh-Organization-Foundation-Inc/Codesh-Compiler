e0ac3ef9031b390b9c994f684a55a853f3aa7710
StavWasPlayZ <stavsolomon101@gmail.com>
2026-01-28 18:14:16 +0200

    Convert recursive function to polymorphistic approach

--- src/parser/ast/method/method_scope_ast_node.cpp
+++ src/parser/ast/method/method_scope_ast_node.cpp
@@ -1,21 +1,6 @@
 #include "method_scope_ast_node.h"
 
 #include "method_declaration_ast_node.h"
-#include "parser/ast/impl/binary_ast_node.h"
-#include "parser/ast/impl/unary_ast_node.h"
-#include "parser/ast/method/operation/block/for_ast_node.h"
-#include "parser/ast/method/operation/block/if_ast_node.h"
-#include "parser/ast/method/operation/block/while_ast_node.h"
-#include "parser/ast/method/operation/method_call_ast_node.h"
-#include "parser/ast/method/operation/return_ast_node.h"
-#include "parser/ast/var_reference/value_ast_node.h"
-
-/**
- * Recursively sets all statement indexes for the given statement and its child statements.
- */
-static void set_all_statement_indexes(codesh::ast::method::operation::method_operation_ast_node *statement,
-        size_t index);
-
 
 const std::optional<std::reference_wrapper<codesh::semantic_analyzer::method_scope_symbol>> &codesh::ast::method::
     method_scope_ast_node::_get_resolved() const
@@ -47,50 +32,8 @@ const std::list<std::unique_ptr<codesh::ast::method::operation::method_operation
 void codesh::ast::method::method_scope_ast_node::add_statement(
         std::unique_ptr<operation::method_operation_ast_node> statement)
 {
-    set_all_statement_indexes(statement.get(), body.size());
+    statement->set_statement_index(body.size());
     body.emplace_back(std::move(statement));
-}
-
-static void set_all_statement_indexes(codesh::ast::method::operation::method_operation_ast_node *statement,
-        const size_t index)
-{
-    if (statement == nullptr)
-        return;
-
-    statement->set_statement_index(index);
-
-    if (const auto *bin_op = dynamic_cast<codesh::ast::impl::binary_ast_node *>(statement))
-    {
-        set_all_statement_indexes(&bin_op->get_left(), index);
-        set_all_statement_indexes(&bin_op->get_right(), index);
-    }
-    else if (const auto *un_op = dynamic_cast<codesh::ast::impl::unary_ast_node *>(statement))
-    {
-        set_all_statement_indexes(&un_op->get_child(), index);
-    }
-    else if (const auto *call_op = dynamic_cast<codesh::ast::method::operation::method_call_ast_node *>(statement))
-    {
-        for (const auto &arg : call_op->get_arguments())
-        {
-            set_all_statement_indexes(arg.get(), index);
-        }
-    }
-    else if (const auto *if_op = dynamic_cast<codesh::ast::block::if_ast_node *>(statement))
-    {
-        set_all_statement_indexes(if_op->get_if_branch().condition.get(), index);
-        for (const auto &else_if : if_op->get_else_if_branches())
-        {
-            set_all_statement_indexes(else_if.condition.get(), index);
-        }
-    }
-    else if (const auto *while_op = dynamic_cast<codesh::ast::block::while_ast_node *>(statement))
-    {
-        set_all_statement_indexes(&while_op->get_condition(), index);
-    }
-    else if (const auto *for_op = dynamic_cast<codesh::ast::block::for_ast_node *>(statement))
-    {
-        set_all_statement_indexes(&for_op->get_collection(), index);
-    }
 }
 
 void codesh::ast::method::method_scope_ast_node::push_front_statement(
