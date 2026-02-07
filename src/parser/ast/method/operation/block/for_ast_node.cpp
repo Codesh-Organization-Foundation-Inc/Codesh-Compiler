84f0a4035391edc07af830adea43d0682b067309
StavWasPlayZ <stavsolomon101@gmail.com>
2026-02-07 20:14:55 +0200

    Code formatting

--- src/parser/ast/method/operation/block/for_ast_node.cpp
+++ src/parser/ast/method/operation/block/for_ast_node.cpp
@@ -2,6 +2,7 @@
 
 #include "parser/ast/collection/range_ast_node.h"
 #include "parser/ast/method/method_scope_ast_node.h"
+#include "parser/ast/var_reference/evaluable_ast_node.h"
 
 codesh::ast::block::for_ast_node::for_ast_node(const blasphemy::code_position code_position,
                            std::unique_ptr<var_reference::value_ast_node> collection,
@@ -28,7 +29,7 @@ codesh::ast::method::method_scope_ast_node &codesh::ast::block::for_ast_node::ge
     return body_scope;
 }
 
-void codesh::ast::block::for_ast_node::set_statement_index(size_t statement_index)
+void codesh::ast::block::for_ast_node::set_statement_index(const size_t statement_index)
 {
     method_operation_ast_node::set_statement_index(statement_index);
     collection->set_statement_index(statement_index);
@@ -46,10 +47,8 @@ void codesh::ast::block::for_ast_node::emit_constants(const compilation_unit_ast
 }
 
 void codesh::ast::block::for_ast_node::emit_ir(
-    output::ir::code_block &containing_block,
-    const semantic_analyzer::symbol_table &symbol_table,
-    const type_decl::type_declaration_ast_node &containing_type_decl
-) const
+        output::ir::code_block &containing_block, const semantic_analyzer::symbol_table &symbol_table,
+        const type_decl::type_declaration_ast_node &containing_type_decl) const
 {
     const auto range_ptr = dynamic_cast<collection::range_ast_node *>(collection.get());
     if (!range_ptr)
@@ -63,26 +62,33 @@ void codesh::ast::block::for_ast_node::emit_ir(
     range_ptr->get_from().emit_ir(containing_block, symbol_table, containing_type_decl);
 
     containing_block.add_instruction(std::make_unique<output::ir::store_in_local_var_instruction>(
-        iterator.get_type()->to_instruction_type(),iterator.get_resolved().get_jvm_index()));
+        iterator.get_type()->to_instruction_type(),iterator.get_resolved().get_jvm_index()
+    ));
 
     // emit body scope
     body_scope.emit_ir(body_block, symbol_table, containing_type_decl);
 
+
+    // Handle skip
+    const auto &skip = range_ptr->get_skip();
     // iterator = iterator + skip
     // load iterator
     body_block.add_instruction(std::make_unique<output::ir::load_instruction>(
-        iterator.get_type()->to_instruction_type(), iterator.get_resolved().get_jvm_index()));
-
+        iterator.get_type()->to_instruction_type(),
+        iterator.get_resolved().get_jvm_index()
+    ));
     // load skip
-    range_ptr->get_skip().emit_ir(body_block, symbol_table, containing_type_decl);
-
+    skip.emit_ir(body_block, symbol_table, containing_type_decl);
     // add
     body_block.add_instruction(std::make_unique<output::ir::operator_instruction>(
-        iterator.get_type()->to_instruction_type(), output::ir::operator_type::ADD));
-
+        iterator.get_type()->to_instruction_type(),
+        output::ir::operator_type::ADD
+    ));
     // store in iterator
     body_block.add_instruction(std::make_unique<output::ir::store_in_local_var_instruction>(
-        iterator.get_type()->to_instruction_type(),iterator.get_resolved().get_jvm_index()));
+        iterator.get_type()->to_instruction_type(),
+        iterator.get_resolved().get_jvm_index()
+    ));
 
     const size_t body_jump_size = body_block.size() + 3; // body + goto
 
@@ -97,7 +103,9 @@ void codesh::ast::block::for_ast_node::emit_ir(
 
     // compare (iterator >= to)
     condition_block.add_instruction(std::make_unique<output::ir::if_instruction>(
-        output::ir::if_type::IS_INT_GREATER_OR_EQUAL, static_cast<int>(body_jump_size)));
+        output::ir::if_type::IS_INT_GREATER_OR_EQUAL,
+        static_cast<int>(body_jump_size))
+    );
 
     const size_t condition_size = condition_block.size();
 
@@ -106,5 +114,6 @@ void codesh::ast::block::for_ast_node::emit_ir(
 
     // Jump back to condition
     containing_block.add_instruction(std::make_unique<output::ir::goto_instruction>(
-            -static_cast<int>(condition_size + body_jump_size)));
+        -static_cast<int>(condition_size + body_jump_size))
+    );
 }
