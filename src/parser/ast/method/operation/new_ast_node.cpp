5fb25d56ab6a88f8dce3ab4e74bf26d4d032c072
Elione <eliranbmo2008@gmail.com>
2026-02-10 22:41:49 +0200

    Finished the IR emission for new_ast_node

--- src/parser/ast/method/operation/new_ast_node.cpp
+++ src/parser/ast/method/operation/new_ast_node.cpp
@@ -1,6 +1,11 @@
 #include "new_ast_node.h"
 
 #include "defenition/definitions.h"
+#include "output/ir/code_block.h"
+#include "output/ir/instruction/dup_instruction.h"
+#include "output/ir/instruction/invoke_instruction.h"
+#include "output/ir/instruction/new_instruction.h"
+#include "parser/ast/type_declaration/type_declaration_ast_node.h"
 #include "semantic_analyzer/symbol_table/symbol_table.h"
 
 codesh::ast::op::new_ast_node::new_ast_node(const blasphemy::code_position code_position,
@@ -28,4 +33,42 @@ void codesh::ast::op::new_ast_node::emit_ir(output::ir::code_block &containing_b
         const semantic_analyzer::symbol_table &symbol_table,
         const type_decl::type_declaration_ast_node &containing_type_decl) const
 {
+    const auto &cp = containing_type_decl.get_constant_pool();
+
+    const auto constructed_class_cpi = cp.get_class_index(
+        cp.get_utf8_index(
+            constructed_type->get_resolved_name().join()
+        )
+    );
+
+    // add new #class_cpi
+    containing_block.add_instruction(std::make_unique<output::ir::new_instruction>(
+        constructed_class_cpi
+    ));
+
+    // add dup
+    containing_block.add_instruction(std::make_unique<output::ir::dup_instruction>());
+
+    // load arguments
+    for (const auto &argument : get_arguments())
+    {
+        argument->emit_ir(containing_block, symbol_table, containing_type_decl);
+    }
+
+    // Call constructor
+    const int method_cpi = cp.get_methodref_index(
+        constructed_class_cpi,
+
+        cp.get_name_and_type_index(
+            cp.get_utf8_index(get_last_name(true)),
+            cp.get_utf8_index(generate_descriptor())
+        )
+    );
+
+    containing_block.add_instruction(std::make_unique<output::ir::invoke_instruction>(
+        output::ir::invokation_type::SPECIAL,
+        method_cpi,
+        get_arguments().size()
+    ));
+
 }
