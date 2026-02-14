c50fd80d9fc483ea29187c2bc6eaf23ffdcc09ef
Elione <eliranbmo2008@gmail.com>
2026-02-14 12:49:21 +0200

    Added interfaces field to type_declaration_ast_node

--- src/parser/ast/type_declaration/type_declaration_ast_node.h
+++ src/parser/ast/type_declaration/type_declaration_ast_node.h
@@ -38,7 +38,7 @@ class type_declaration_ast_node : public impl::ast_node, public impl::i_descript
     std::unique_ptr<output::jvm_target::constant_pool> constant_pool;
 
     std::unique_ptr<type::custom_type_ast_node> super_class;
-    //TODO: Add implements
+    std::vector<std::unique_ptr<type::custom_type_ast_node>> interfaces;
 
     const definition::fully_qualified_name name;
     std::optional<std::reference_wrapper<semantic_analyzer::type_symbol>> resolved_symbol;
