748269ab630dd0c906e827b289c32615dd5e5f34
Elione <eliranbmo2008@gmail.com>
2026-02-21 13:16:18 +0200

    Made some of the methods in field_declaration_ast_node.cpp

--- src/parser/ast/type_declaration/field_declaration_ast_node.cpp
+++ src/parser/ast/type_declaration/field_declaration_ast_node.cpp
@@ -1,1 +1,39 @@
 #include "field_declaration_ast_node.h"
+
+namespace codesh::ast::type_decl
+{
+
+    field_declaration_ast_node::field_declaration_ast_node(blasphemy::code_position pos,
+            definition::fully_qualified_name name, std::unique_ptr<type::type_ast_node> field_type) :
+        ast_node(pos),
+        name(std::move(name)),
+        field_type(std::move(field_type))
+    {
+    }
+
+    const definition::fully_qualified_name& field_declaration_ast_node::get_name() const
+    {
+        return name;
+    }
+
+    type::type_ast_node* field_declaration_ast_node::get_type() const
+    {
+        return field_type.get();
+    }
+
+    std::string field_declaration_ast_node::generate_descriptor(bool resolved) const
+    {
+        return field_type->generate_descriptor(resolved);
+    }
+
+    void field_declaration_ast_node::emit_constants(const compilation_unit_ast_node&,
+        output::jvm_target::constant_pool& constant_pool)
+    {
+    }
+
+    void field_declaration_ast_node::set_resolved(semantic_analyzer::field_symbol& symbol)
+    {
+        resolved_symbol.emplace(symbol);
+    }
+
+}
