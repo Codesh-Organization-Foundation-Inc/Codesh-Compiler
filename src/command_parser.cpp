0bb9debbdf257900bbe0ebc9513d4a77f189d46a
Elione <eliranbmo2008@gmail.com>
2026-02-23 20:10:51 +0200

    Added the classpath parsing to command_parser and modified command_args struct

--- src/command_parser.cpp
+++ src/command_parser.cpp
@@ -20,5 +20,51 @@ codesh::command_args codesh::parse_command(const int argc, char **argv)
     result.src_path = argv[1];
     result.dest_path = argv[2];
 
+    if (argc == 3)
+    {
+        return result;
+    }
+
+    for (int i = 3; i < argc; ++i)
+    {
+        std::string arg = argv[i];
+
+        if (arg == "--classpath")
+        {
+            if (i + 1 >= argc)
+                blasphemy::get_blasphemy_collector().add_blasphemy(
+                    blasphemy::details::NO_CLASSPATH_ARG,
+                    blasphemy::blasphemy_type::INIT,
+                    blasphemy::NO_CODE_POS,
+                    false
+                );
+
+            std::stringstream stream(argv[++i]);
+            std::string entry;
+
+            while (std::getline(stream, entry, ':'))
+            {
+                std::filesystem::path file_path(entry);
+
+                if (std::filesystem::is_directory(file_path)) // add is_zip check
+                {
+                    result.classpath.push_back(file_path);
+                }
+                else
+                {
+                    blasphemy::blasphemy_collector().add_blasphemy(
+                        fmt::format(
+                            blasphemy::details::INVALID_CLASSPATH_ARG,
+                            entry
+                        ),
+                        blasphemy::blasphemy_type::INIT,
+                        blasphemy::NO_CODE_POS,
+                        false
+                    );
+                }
+            }
+        }
+    }
+
     return result;
 }
