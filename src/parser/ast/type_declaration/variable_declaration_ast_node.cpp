a86ea9fb9f01de0941857eaaaff4d50960297025
Elione <eliranbmo2008@gmail.com>
2026-02-23 17:55:48 +0200

    Fixed polymorphism issues and the namespace

--- src/parser/ast/type_declaration/variable_declaration_ast_node.cpp
+++ src/parser/ast/type_declaration/variable_declaration_ast_node.cpp
@@ -2,43 +2,38 @@
 #include "parser/ast/type/custom_type_ast_node.h"
 
 
-codesh::ast::variable_declaration_ast_node::variable_declaration_ast_node(
-        blasphemy::code_position pos,
-        std::string name,
-        std::unique_ptr<type::type_ast_node> type)
-    : ast_node(pos),
-      name(std::move(name)),
-      type(std::move(type))
+codesh::ast::type_decl::variable_declaration_ast_node::variable_declaration_ast_node(
+        blasphemy::code_position pos) : ast_node(std::move(pos))
 {
 }
 
-const std::string& codesh::ast::variable_declaration_ast_node::get_name() const
+const std::string& codesh::ast::type_decl::variable_declaration_ast_node::get_name() const
 {
     return name;
 }
 
-void codesh::ast::variable_declaration_ast_node::set_name(const std::string& name)
+void codesh::ast::type_decl::variable_declaration_ast_node::set_name(const std::string& name)
 {
     this->name = name;
 }
 
-codesh::ast::type::type_ast_node* codesh::ast::variable_declaration_ast_node::get_type() const
+codesh::ast::type::type_ast_node* codesh::ast::type_decl::variable_declaration_ast_node::get_type() const
 {
     return type.get();
 }
 
-void codesh::ast::variable_declaration_ast_node::set_type(std::unique_ptr<type::type_ast_node> type)
+void codesh::ast::type_decl::variable_declaration_ast_node::set_type(std::unique_ptr<type::type_ast_node> type)
 {
     this->type = std::move(type);
 }
 
-codesh::ast::type_decl::attributes_ast_node* codesh::ast::variable_declaration_ast_node::get_attributes() const
+codesh::ast::type_decl::attributes_ast_node* codesh::ast::type_decl::variable_declaration_ast_node::get_attributes() const
 {
     return attributes.get();
 }
 
-void codesh::ast::variable_declaration_ast_node::set_attributes(
-        std::unique_ptr<type_decl::attributes_ast_node> attributes)
+void codesh::ast::type_decl::variable_declaration_ast_node::set_attributes(
+        std::unique_ptr<attributes_ast_node> attributes)
 {
     this->attributes = std::move(attributes);
 }
--- src/parser/ast/type_declaration/variable_declaration_ast_node.h
+++ src/parser/ast/type_declaration/variable_declaration_ast_node.h
@@ -1,42 +1,34 @@
 #pragma once
 
-#pragma once
-
 #include "parser/ast/impl/ast_node.h"
 #include "parser/ast/impl/i_constant_pool_emitter.h"
 #include "parser/ast/type/type_ast_node.h"
 #include "parser/ast/type_declaration/attributes_ast_node.h"
 
-#include <memory>
-#include <string>
-
-namespace codesh::ast //FIXME: change the namespace to the correct one if i need to
+namespace codesh::ast::type_decl
 {
 
 class variable_declaration_ast_node : public impl::ast_node, public impl::i_constant_pool_emitter
 {
-
-protected:
     std::string name;
     std::unique_ptr<type::type_ast_node> type;
-    std::unique_ptr<type_decl::attributes_ast_node> attributes;
+    std::unique_ptr<attributes_ast_node> attributes;
 
-    variable_declaration_ast_node(blasphemy::code_position pos, std::string name,
-            std::unique_ptr<type::type_ast_node> type);
+protected:
+    explicit variable_declaration_ast_node(blasphemy::code_position pos);
 
-    void emit_common_constants(output::jvm_target::constant_pool& constant_pool) const;
+    void emit_constants(const compilation_unit_ast_node &root_node,
+            output::jvm_target::constant_pool &constant_pool) override;
 
 public:
-    virtual ~variable_declaration_ast_node() = default;
-
     [[nodiscard]] const std::string& get_name() const;
     void set_name(const std::string& name);
 
     [[nodiscard]] type::type_ast_node* get_type() const;
     void set_type(std::unique_ptr<type::type_ast_node> type);
 
-    [[nodiscard]] type_decl::attributes_ast_node* get_attributes() const;
-    void set_attributes(std::unique_ptr<type_decl::attributes_ast_node> attributes);
+    [[nodiscard]] attributes_ast_node* get_attributes() const;
+    void set_attributes(std::unique_ptr<attributes_ast_node> attributes);
 };
 
 }
