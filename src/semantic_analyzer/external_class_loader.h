ae5461d19636d5bfd92c4150712a339c7649d4e6
StavWasPlayZ <stavsolomon101@gmail.com>
2026-02-24 22:42:43 +0200

    Add docs to load_external_class_file

--- src/semantic_analyzer/external_class_loader.h
+++ src/semantic_analyzer/external_class_loader.h
@@ -10,5 +10,8 @@ class symbol_table;
 
 namespace codesh::semantic_analyzer
 {
+/**
+ * Loads all symbols of an external class file to the provided @p table
+ */
 void load_external_class_file(const std::filesystem::path &path, symbol_table &table);
 }
