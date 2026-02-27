1cbbbc0cc4f3098f5dca6853bd1848aa9ef07fc7
StavWasPlayZ <stavsolomon101@gmail.com>
2026-02-27 17:06:21 +0200

    Add putfield instruction

--- src/output/ir/instruction/impl/opcode.h
+++ src/output/ir/instruction/impl/opcode.h
@@ -32,6 +32,7 @@ enum class opcode : unsigned char
     GET_STATIC = 0xB2,
     PUT_STATIC = 0xB3,
     GET_FIELD  = 0xB4,
+    PUT_FIELD  = 0xB5,
 
     INVOKE_DYNAMIC = 0xBA,
     INVOKE_INTERFACE = 0xB9,
