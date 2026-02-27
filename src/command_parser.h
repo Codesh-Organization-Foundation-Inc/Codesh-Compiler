f7a76371b1ee01b49da6e20d2a3696cf152e5d87
Elione <eliranbmo2008@gmail.com>
2026-02-27 22:09:50 +0200

    Added the default classpaths arguments flags

--- src/command_parser.h
+++ src/command_parser.h
@@ -12,6 +12,9 @@ struct command_args
     std::filesystem::path dest_path;
 
     std::vector<std::filesystem::path> classpath;
+
+    bool is_java_default_classpath = true; // disabled by --sinful
+    bool is_talmud_codesh_classpath = true; // disabled by --unholy
 };
 
 command_args parse_command(int argc, char **argv);
