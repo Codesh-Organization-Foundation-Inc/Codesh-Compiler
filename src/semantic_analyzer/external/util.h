ddb9d9fd5ce93b2279640e0d3cac0a1c453c0a76
StavWasPlayZ <stavsolomon101@gmail.com>
2026-03-02 12:29:32 +0200

    Remove static keyword from util's readers

--- src/semantic_analyzer/external/util.h
+++ src/semantic_analyzer/external/util.h
@@ -6,8 +6,8 @@
 namespace codesh::semantic_analyzer::external::util
 {
 
-static uint8_t read_u1(std::ifstream &file);
-static uint16_t read_u2(std::ifstream &file);
-static uint32_t read_u4(std::ifstream &file);
+uint8_t read_u1(std::ifstream &file);
+uint16_t read_u2(std::ifstream &file);
+uint32_t read_u4(std::ifstream &file);
 
 }
