d6505efb99949306350c089f1c2b7738f2f5487a
Elione <eliranbmo2008@gmail.com>
2026-03-05 20:17:36 +0200

    Added const to getter and adder code_position to constructor

--- src/parser/ast/operator/assignment/cast_ast_node.h
+++ src/parser/ast/operator/assignment/cast_ast_node.h
@@ -12,10 +12,10 @@ class cast_ast_node : public var_reference::value_ast_node
     type::type_ast_node* right;
 
 public:
-    cast_ast_node(std::unique_ptr<value_ast_node> left,
-                  type::type_ast_node* right);
+    cast_ast_node(blasphemy::code_position code_position, std::unique_ptr<value_ast_node> left,
+            type::type_ast_node* right);
 
-    [[nodiscard]] value_ast_node& get_left() const;
+    [[nodiscard]] const value_ast_node& get_left() const;
     [[nodiscard]] type::type_ast_node* get_right() const;
 
     [[nodiscard]] type::type_ast_node* get_type() const override;
