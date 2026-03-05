c292333b83857da8091e8af12065115480518c78
Elione <eliranbmo2008@gmail.com>
2026-03-05 20:09:24 +0200

    Modified cast_ast_node.h so it will inherit value_ast_node

--- src/parser/ast/operator/assignment/cast_ast_node.h
+++ src/parser/ast/operator/assignment/cast_ast_node.h
@@ -1,25 +1,24 @@
 #pragma once
-#include "parser/ast/impl/binary_ast_node.h"
-#include "parser/ast/var_reference/variable_reference_ast_node.h"
+
+#include "parser/ast/var_reference/value_ast_node.h"
+#include "parser/ast/type/type_ast_node.h"
 
 namespace codesh::ast::op::assignment
 {
 
-class cast_ast_node : public impl::binary_ast_node
+class cast_ast_node : public var_reference::value_ast_node
 {
     std::unique_ptr<value_ast_node> left;
-    std::unique_ptr<type::type_ast_node> right;
+    type::type_ast_node* right;
 
 public:
-    cast_ast_node(
-        std::unique_ptr<value_ast_node> left,
-        std::unique_ptr<type::type_ast_node> right);
+    cast_ast_node(std::unique_ptr<value_ast_node> left,
+                  type::type_ast_node* right);
 
-    [[nodiscard]] value_ast_node& get_left();
-    [[nodiscard]] const value_ast_node& get_left() const;
+    [[nodiscard]] value_ast_node& get_left() const;
+    [[nodiscard]] type::type_ast_node* get_right() const;
 
-    [[nodiscard]] type::type_ast_node& get_right();
-    [[nodiscard]] const type::type_ast_node& get_right() const;
+    [[nodiscard]] type::type_ast_node* get_type() const override;
 };
 
 }
