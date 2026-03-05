3cf14bbb469e6995e6cb69c0d799a9edc4eee3d2
Elione <eliranbmo2008@gmail.com>
2026-03-05 19:35:27 +0200

    Changed type of value parameter

--- src/parser/ast/operator/assignment/cast_ast_node.h
+++ src/parser/ast/operator/assignment/cast_ast_node.h
@@ -7,16 +7,16 @@ namespace codesh::ast::op::assignment
 
 class cast_ast_node : public impl::binary_ast_node
 {
-    std::unique_ptr<variable_reference_ast_node> value;
+    std::unique_ptr<value_ast_node> value;
     std::unique_ptr<type::type_ast_node> type;
 
 public:
     cast_ast_node(
-        std::unique_ptr<variable_reference_ast_node> value,
+        std::unique_ptr<value_ast_node> value,
         std::unique_ptr<type::type_ast_node> target_type);
 
-    variable_reference_ast_node& get_value();
-    const variable_reference_ast_node& get_value() const;
+    value_ast_node& get_value();
+    const value_ast_node& get_value() const;
 
     type::type_ast_node& get_target_type();
     const type::type_ast_node& get_target_type() const;
