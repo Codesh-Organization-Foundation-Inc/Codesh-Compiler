4254ee619a4ba332d27bfafbf2b2437b3db8cbed
StavWasPlayZ <stavsolomon101@gmail.com>
2026-03-08 00:41:46 +0200

    Fix convention issues

--- src/parser/ast/operator/assignment/cast_ast_node.cpp
+++ src/parser/ast/operator/assignment/cast_ast_node.cpp
@@ -1,31 +1,30 @@
 #include "cast_ast_node.h"
 
-codesh::ast::op::assignment::cast_ast_node::cast_ast_node(blasphemy::code_position code_position,
-        std::unique_ptr<value_ast_node> left, type::type_ast_node* right) :
+codesh::ast::op::assignment::cast_ast_node::cast_ast_node(const blasphemy::code_position code_position,
+        std::unique_ptr<value_ast_node> left, type::type_ast_node *right) :
     value_ast_node(code_position),
     left(std::move(left)),
     right(right)
 {
 }
 
-const codesh::ast::var_reference::value_ast_node& codesh::ast::op::assignment::cast_ast_node::get_left() const
+const codesh::ast::var_reference::value_ast_node &codesh::ast::op::assignment::cast_ast_node::get_left() const
 {
     return *left;
 }
 
-codesh::ast::type::type_ast_node* codesh::ast::op::assignment::cast_ast_node::get_right() const
+codesh::ast::type::type_ast_node *codesh::ast::op::assignment::cast_ast_node::get_right() const
 {
     return right;
 }
 
-codesh::ast::type::type_ast_node* codesh::ast::op::assignment::cast_ast_node::get_type() const
+codesh::ast::type::type_ast_node *codesh::ast::op::assignment::cast_ast_node::get_type() const
 {
     return right;
 }
 
-void codesh::ast::op::assignment::cast_ast_node::emit_ir(output::ir::code_block& containing_block,
+void codesh::ast::op::assignment::cast_ast_node::emit_ir(output::ir::code_block &containing_block,
     const semantic_analyzer::symbol_table& symbol_table,
     const type_decl::type_declaration_ast_node& containing_type_decl) const
 {
 }
-
--- src/parser/ast/operator/assignment/cast_ast_node.h
+++ src/parser/ast/operator/assignment/cast_ast_node.h
@@ -9,19 +9,19 @@ namespace codesh::ast::op::assignment
 class cast_ast_node : public var_reference::value_ast_node
 {
     std::unique_ptr<value_ast_node> left;
-    type::type_ast_node* right;
+    type::type_ast_node *right;
 
 public:
     cast_ast_node(blasphemy::code_position code_position, std::unique_ptr<value_ast_node> left,
-            type::type_ast_node* right);
+            type::type_ast_node *right);
 
-    [[nodiscard]] const value_ast_node& get_left() const;
-    [[nodiscard]] type::type_ast_node* get_right() const;
+    [[nodiscard]] const value_ast_node &get_left() const;
+    [[nodiscard]] type::type_ast_node *get_right() const;
 
-    [[nodiscard]] type::type_ast_node* get_type() const override;
+    [[nodiscard]] type::type_ast_node *get_type() const override;
 
-    void emit_ir(output::ir::code_block& containing_block, const semantic_analyzer::symbol_table& symbol_table,
-        const type_decl::type_declaration_ast_node& containing_type_decl) const override;
+    void emit_ir(output::ir::code_block &containing_block, const semantic_analyzer::symbol_table &symbol_table,
+        const type_decl::type_declaration_ast_node &containing_type_decl) const override;
 };
 
 }
