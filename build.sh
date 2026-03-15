4195f90a2b3a5b20beffd09d6af5007a40399ad4
StavWasPlayZ <stavsolomon101@gmail.com>
2026-03-15 17:33:59 +0200

    Add missing shebang line

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if (( EUID != 0 )); then
     echo "וִיבַקֵּשׁ ה' לְהָרִץ אֶת־הַסְּקְרִיפְּט בְּסַמְכוּת שֹׁרֶשׁ"
     exit 1
@@ -18,8 +20,6 @@ cmake --install ./build --prefix $CODESH_PATH
 # Link to the compiler in bin
 ln -sf $CODESH_EXECUTABLE /usr/local/bin/codeshc
 
-
-#TODO: Compress Talmud Codesh into JAR file after JAR loading is implemented
 
 mkdir -p $TALMUD_CODESH_PATH
 $CODESH_EXECUTABLE ./resources/lib-src/ $TALMUD_CODESH_PATH --sinful
