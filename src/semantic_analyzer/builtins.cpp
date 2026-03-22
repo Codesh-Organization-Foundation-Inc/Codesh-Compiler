565fc0f491277a64678224582b845f1ddde6f35e
StavWasPlayZ <stavsolomon101@gmail.com>
2026-03-23 00:29:20 +0200

    Nest Codesh aliases within ישראל ל־קודש ל־בן ל־משה

--- src/semantic_analyzer/builtins.cpp
+++ src/semantic_analyzer/builtins.cpp
@@ -5,17 +5,45 @@
 #include "semantic_analyzer/symbol_table/symbol.h"
 #include "semantic_analyzer/symbol_table/symbol_table.h"
 
+static codesh::semantic_analyzer::country_symbol &talmud_codesh_country(
+        const codesh::semantic_analyzer::symbol_table &table);
+static codesh::semantic_analyzer::country_symbol &nested_country(codesh::semantic_analyzer::country_symbol &parent,
+        const std::string &name, const std::string &bin_fqn);
+
 static void add_alias_ktuvim(codesh::semantic_analyzer::country_symbol &country);
 static void add_alias_labubu(codesh::semantic_analyzer::country_symbol &country);
 
 
 void codesh::semantic_analyzer::builtins::collect_builtins(const symbol_table &table)
 {
-    //TODO: Properly wrap in Talmud Codesh country
-    country_symbol &country = table.get_global_scope();
+    auto &country = talmud_codesh_country(table);
 
     add_alias_ktuvim(country);
     add_alias_labubu(country);
+}
+
+static codesh::semantic_analyzer::country_symbol &talmud_codesh_country(
+        const codesh::semantic_analyzer::symbol_table &table)
+{
+    codesh::semantic_analyzer::country_symbol &global = table.get_global_scope();
+    codesh::semantic_analyzer::country_symbol &israel = nested_country(global, "ישראל", "ישראל");
+    codesh::semantic_analyzer::country_symbol &codesh = nested_country(israel, "קודש","ישראל/קודש");
+    codesh::semantic_analyzer::country_symbol &ben = nested_country(codesh, "בן", "ישראל/קודש/בן");
+    codesh::semantic_analyzer::country_symbol &moshe = nested_country(ben, "משה", "ישראל/קודש/בן/משה");
+
+    return moshe;
+}
+
+static codesh::semantic_analyzer::country_symbol &nested_country(
+        codesh::semantic_analyzer::country_symbol &parent, const std::string &name, const std::string &bin_fqn)
+{
+    return parent.get_scope().add_symbol(
+        name,
+        std::make_unique<codesh::semantic_analyzer::country_symbol>(
+            codesh::definition::fully_qualified_name::parse(bin_fqn, codesh::lexer::NO_CODE_POS),
+            &parent
+        )
+    ).first.get();
 }
 
 static void add_alias_ktuvim(codesh::semantic_analyzer::country_symbol &country)
