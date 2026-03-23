384fb1ca391a9eb31e7092e2d1f597ac039a9717
Elione <eliranbmo2008@gmail.com>
2026-03-23 19:50:59 +0200

    Changed interface_declaration_ast_node to inherit from type_declaration_ast_node

--- src/parser/ast/type_declaration/interface_declaration_ast_node.cpp
+++ src/parser/ast/type_declaration/interface_declaration_ast_node.cpp
@@ -1,83 +1,16 @@
 #include "interface_declaration_ast_node.h"
 
-codesh::ast::type_decl::interface_declaration_ast_node::interface_declaration_ast_node(
-        lexer::code_position code_position, definition::fully_qualified_name name) :
-    ast_node(code_position), name(std::move(name))
-{
-}
+#include "parser/ast/type/custom_type_ast_node.h"
+#include "semantic_analyzer/builtins.h"
 
-codesh::ast::type_decl::interface_declaration_ast_node::~interface_declaration_ast_node() = default;
-
-void codesh::ast::type_decl::interface_declaration_ast_node::set_resolved(semantic_analyzer::type_symbol &symbol)
+interface_declaration_ast_node::interface_declaration_ast_node(codesh::lexer::code_position code_position,
+                                                               codesh::definition::fully_qualified_name name) :
+    type_declaration_ast_node(code_position, std::move(name))
 {
-    resolved_symbol = symbol;
-}
+    auto object_fqn = codesh::definition::fully_qualified_name::parse(
+        codesh::semantic_analyzer::builtins::ALIAS_OBJECT, code_position);
 
-const std::optional<std::reference_wrapper<codesh::semantic_analyzer::type_symbol>> &codesh::ast::type_decl::
-        interface_declaration_ast_node::_get_resolved() const
-{
-    return resolved_symbol;
-}
+    auto custom_type_node = std::make_unique<codesh::ast::type::custom_type_ast_node>(code_position, object_fqn);
 
-const codesh::definition::fully_qualified_name &codesh::ast::type_decl::
-        interface_declaration_ast_node::get_unresolved_name() const
-{
-    return name;
-}
-
-std::string codesh::ast::type_decl::interface_declaration_ast_node::generate_descriptor(bool resolved) const
-{
-    return nullptr;
-}
-
-void codesh::ast::type_decl::interface_declaration_ast_node::add_extended_interface(
-        std::unique_ptr<type::custom_type_ast_node> interface)
-{
-    extended_interfaces.push_back(std::move(interface));
-}
-
-const std::vector<std::unique_ptr<codesh::ast::type::custom_type_ast_node>> &codesh::ast::type_decl::
-        interface_declaration_ast_node::get_extended_interfaces() const
-{
-    return extended_interfaces;
-}
-
-void codesh::ast::type_decl::interface_declaration_ast_node::add_method(
-        std::unique_ptr<method::method_declaration_ast_node> method)
-{
-    methods.push_back(std::move(method));
-}
-
-const std::deque<std::unique_ptr<codesh::ast::method::method_declaration_ast_node>> &codesh::ast::type_decl::
-        interface_declaration_ast_node::get_methods() const
-{
-    return methods;
-}
-
-void codesh::ast::type_decl::interface_declaration_ast_node::set_attributes(
-        std::unique_ptr<attributes_ast_node> attr)
-{
-    attributes = std::move(attr);
-}
-
-codesh::ast::type_decl::attributes_ast_node *codesh::ast::type_decl::
-        interface_declaration_ast_node::get_attributes() const
-{
-    return attributes.get();
-}
-
-const codesh::output::jvm_target::constant_pool &codesh::ast::type_decl::interface_declaration_ast_node::
-        get_constant_pool() const
-{
-    return *constant_pool;
-}
-
-void codesh::ast::type_decl::interface_declaration_ast_node::set_constant_pool(
-        output::jvm_target::constant_pool constant_pool)
-{
-}
-
-void codesh::ast::type_decl::interface_declaration_ast_node::emit_constants(const compilation_unit_ast_node& root_node,
-        output::jvm_target::constant_pool& constant_pool)
-{
+    set_super_class(std::move(custom_type_node));
 }
