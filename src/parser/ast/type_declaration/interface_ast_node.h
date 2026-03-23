909824b0fa417b90bbb5c9d5c0c864c5032e91c0
Elione <eliranbmo2008@gmail.com>
2026-03-23 04:20:07 +0200

    Modified according to conventions

--- src/parser/ast/type_declaration/interface_ast_node.h
+++ src/parser/ast/type_declaration/interface_ast_node.h
@@ -43,32 +43,24 @@ protected:
 
 public:
     interface_declaration_ast_node(lexer::code_position code_position, definition::fully_qualified_name name);
-
     ~interface_declaration_ast_node() override;
 
     void set_resolved(semantic_analyzer::type_symbol &symbol) override;
 
     [[nodiscard]] const definition::fully_qualified_name &get_unresolved_name() const override;
 
-    using i_descriptor_emitter::generate_descriptor;
     [[nodiscard]] std::string generate_descriptor(bool resolved) const override;
 
     void add_extended_interface(std::unique_ptr<type::custom_type_ast_node> interface);
-
-    [[nodiscard]] const std::vector<std::unique_ptr<type::custom_type_ast_node>> &
-    get_extended_interfaces() const;
+    [[nodiscard]] const std::vector<std::unique_ptr<type::custom_type_ast_node>> &get_extended_interfaces() const;
 
     void add_method(std::unique_ptr<method::method_declaration_ast_node> method);
-
-    [[nodiscard]] const std::deque<std::unique_ptr<method::method_declaration_ast_node>> &
-    get_methods() const;
+    [[nodiscard]] const std::deque<std::unique_ptr<method::method_declaration_ast_node>> &get_methods() const;
 
     void set_attributes(std::unique_ptr<attributes_ast_node> attributes);
-
     [[nodiscard]] attributes_ast_node *get_attributes() const;
 
     [[nodiscard]] const output::jvm_target::constant_pool &get_constant_pool() const;
-
     void set_constant_pool(output::jvm_target::constant_pool constant_pool);
 
     void emit_constants(const compilation_unit_ast_node &root_node,
