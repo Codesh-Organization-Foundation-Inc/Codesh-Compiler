11318ac107c55f671bc1b12faf07c5982fc042be
StavWasPlayZ <stavsolomon101@gmail.com>
2026-03-25 20:20:38 +0200

    Fix not consuming "the" keyword of import declarations

--- src/parser/import_parser.cpp
+++ src/parser/import_parser.cpp
@@ -16,9 +16,9 @@ std::unique_ptr<ast::import_declaration_ast_node> codesh::parser::parse_import(
         import_pos
     );
 
-    // Check if is a static import
-    util::ensure_tokens_exist(tokens, blasphemy::details::NO_IMPORT_TYPE); // TODO: provide better reason
-    switch (tokens.front()->get_group())
+    // Static/regular import
+    const auto token = util::consume_token(tokens, blasphemy::details::NO_IMPORT_TYPE); // TODO: provide better reason
+    switch (token->get_group())
     {
     case token_group::KEYWORD_IMPORT_STATIC:
         import_node->set_is_static(true);
@@ -29,8 +29,10 @@ std::unique_ptr<ast::import_declaration_ast_node> codesh::parser::parse_import(
         break;
 
     default:
-        blasphemy::get_blasphemy_collector().add_blasphemy(blasphemy::details::NO_IMPORT_TYPE,
-            blasphemy::blasphemy_type::SYNTAX, tokens.front()->get_code_position());
+        blasphemy::get_blasphemy_collector().add_blasphemy(
+            blasphemy::details::NO_IMPORT_TYPE,
+            blasphemy::blasphemy_type::SYNTAX, tokens.front()->get_code_position()
+        );
     }
 
     util::parse_fqn(tokens, import_node->get_package_name());
