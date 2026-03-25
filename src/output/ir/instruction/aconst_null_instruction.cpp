e0591cd41a480ee1580d8aac004591398e8cc62f
StavWasPlayZ <stavsolomon101@gmail.com>
2026-03-25 23:27:47 +0200

    Formatting fix

--- src/output/ir/instruction/aconst_null_instruction.cpp
+++ src/output/ir/instruction/aconst_null_instruction.cpp
@@ -1,5 +1,6 @@
 #include "aconst_null_instruction.h"
 
-codesh::output::ir::aconst_null_instruction::aconst_null_instruction() : simple_instruction(opcode::ACONST_NULL, 1, 1)
+codesh::output::ir::aconst_null_instruction::aconst_null_instruction() :
+    simple_instruction(opcode::ACONST_NULL, 1, 1)
 {
 }
