fb27cd3d6bb083d256c9a5351efdfa462116b7d9
StavWasPlayZ <stavsolomon101@gmail.com>
2026-03-25 23:25:37 +0200

    Parse null tokens

--- src/parser/value/value_parser.cpp
+++ src/parser/value/value_parser.cpp
@@ -20,6 +20,7 @@
 #include "../ast/operator/cast/manual_cast_ast_node.h"
 #include "fmt/format.h"
 #include "parser/ast/var_reference/array_access_ast_node.h"
+#include "parser/ast/var_reference/null_value_ast_node.h"
 #include "parser/type/class/method_parser.h"
 #include "parser/util.h"
 #include "token/token_group.h"
@@ -147,6 +148,13 @@ std::unique_ptr<codesh::ast::var_reference::value_ast_node> codesh::parser::valu
     case token_group::KEYWORD_FUNCTION_CALL:
         lhs = parse_method_call(tokens);
         break;
+
+    case token_group::KEYWORD_NULL: {
+        lhs = std::make_unique<ast::var_reference::null_value_ast_node>(
+            util::consume_token(tokens)->get_code_position()
+        );
+        break;
+    }
 
     // New operator
     case token_group::KEYWORD_NEW:
