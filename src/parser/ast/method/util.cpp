4892293ed8258251038bd1929ebf36d80961d372
StavWasPlayZ <stavsolomon101@gmail.com>
2026-03-28 19:08:50 +0300

    Add util for pretty-printing method signatures

--- src/parser/ast/method/util.cpp
+++ src/parser/ast/method/util.cpp
@@ -1,11 +1,20 @@
 #include "util.h"
 
+#include "parser/ast/local_variable_declaration_ast_node.h"
+#include "parser/ast/method/method_declaration_ast_node.h"
+#include "parser/ast/type/primitive_type_ast_node.h"
+#include "semantic_analyzer/symbol_table/symbol.h"
+
 #include "fmt/xchar.h"
 
 #include <ranges>
 
 [[nodiscard]] static std::string generate_unresolved_parameter_descriptors(
         const std::deque<std::reference_wrapper<codesh::ast::type::type_ast_node>> &parameter_types);
+[[nodiscard]] static std::string generate_signature_base_name(const codesh::semantic_analyzer::method_symbol &method);
+[[nodiscard]] static std::optional<std::string> generate_signature_params(
+        const codesh::semantic_analyzer::method_symbol &method);
+[[nodiscard]] static bool has_non_void_return(const codesh::ast::type::type_ast_node &return_type);
 
 
 std::string codesh::ast::method::util::generate_method_descriptor(
@@ -57,6 +66,71 @@ std::string codesh::ast::method::util::generate_parameters_descriptor(
     }
 
     return fmt::to_string(out);
+}
+
+std::string codesh::ast::method::util::pretty_method_signature(const semantic_analyzer::method_symbol &method)
+{
+    std::string signature = generate_signature_base_name(method);
+
+    if (const auto params = generate_signature_params(method))
+    {
+        signature += *params;
+    }
+
+    if (has_non_void_return(method.get_return_type()))
+    {
+        signature += fmt::format(" וַיָּשָׁב {}", method.get_return_type().to_pretty_string());
+    }
+
+    return signature;
+}
+
+static std::string generate_signature_base_name(const codesh::semantic_analyzer::method_symbol &method)
+{
+    return fmt::format("מַעֲשֶׂה וּשְׁמוֹ {}", method.get_full_name().get_last_part());
+}
+
+static std::optional<std::string> generate_signature_params(const codesh::semantic_analyzer::method_symbol &method)
+{
+    const auto &parameter_types = method.get_parameter_types();
+    if (parameter_types.empty())
+        return std::nullopt;
+
+    const auto *producing_node = method.get_producing_node();
+    const bool is_static = method.get_attributes().get_is_static();
+
+    std::vector<std::string> param_strings;
+    for (size_t i = 0; i < parameter_types.size(); ++i)
+    {
+        if (i == 0 && !is_static)
+            continue;
+
+        if (producing_node == nullptr)
+        {
+            param_strings.push_back(parameter_types.at(i)->to_pretty_string());
+        }
+        else
+        {
+            param_strings.push_back(
+                fmt::format(
+                    "{} וּשְׁמוֹ {}",
+                    parameter_types.at(i)->to_pretty_string(),
+                    producing_node->get_parameters().at(i).get().get_name()
+                )
+            );
+        }
+    }
+
+    return fmt::format(
+        " וַיִּקַּח {}",
+        fmt::join(param_strings, " וַיִּקַּח ")
+    );
+}
+
+static bool has_non_void_return(const codesh::ast::type::type_ast_node &return_type)
+{
+    const auto *prim = dynamic_cast<const codesh::ast::type::primitive_type_ast_node *>(&return_type);
+    return prim == nullptr || prim->get_type() != codesh::definition::primitive_type::VOID;
 }
 
 static std::string generate_unresolved_parameter_descriptors(
