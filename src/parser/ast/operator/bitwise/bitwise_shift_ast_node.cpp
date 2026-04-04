ec27f77da6c9490db3609a7ae5482bbff02f7bb2
Elione <eliranbmo2008@gmail.com>
2026-04-04 22:21:52 +0300

    Made the h and cpp of the bitwise operators

--- src/parser/ast/operator/bitwise/bitwise_shift_ast_node.cpp
+++ src/parser/ast/operator/bitwise/bitwise_shift_ast_node.cpp
@@ -1,1 +1,14 @@
 #include "bitwise_shift_ast_node.h"
+
+codesh::ast::op::bitwise_shift_ast_node::bitwise_shift_ast_node(const lexer::code_position code_position,
+                                                                std::unique_ptr<value_ast_node> left,
+                                                                std::unique_ptr<value_ast_node> right,
+                                                                const direction shift_direction) :
+    binary_ast_node(code_position, std::move(left), std::move(right)), shift_direction(shift_direction)
+{
+}
+
+std::string codesh::ast::op::bitwise_shift_ast_node::to_pretty_string() const
+{
+    return nullptr;
+}
